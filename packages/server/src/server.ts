634122476befaa9eb03ef3ada96f804e094dfa83
Peter An <75343007+Exotica0122@users.noreply.github.com>
2023-03-30 22:31:08 +1300

    basic backend init

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,6 +2,10 @@ import { app } from "./app";
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res, next) => {
+    res.status(200).json({ msg: "Hello World!" });
+});
+
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 );
