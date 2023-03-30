eb950e0a27b2e9f66d1583b1841c44dbdcbb01bb
aguo921 <aguo921@aucklanduni.ac.nz>
2023-03-30 21:18:26 +1300

    Implement text input functionality

--- packages/client/src/components/TextInput/TextInput.tsx
+++ packages/client/src/components/TextInput/TextInput.tsx
@@ -1,5 +1,19 @@
-const TextInput = () => {
-    return <div>Test</div>
+import TextInputProps from "./TextInputProps"
+
+const TextInput = (props: TextInputProps) => {
+    return (
+        <div>
+            <label>
+                {props.label}
+                <input
+                    type="text"
+                    defaultValue={props.value}
+                    placeholder={props.placeholder}
+                    onChange={(e) => props.onChange(e.target.value)}
+                />
+            </label>
+        </div>
+    )
 }
 
 export default TextInput
