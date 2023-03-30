fe3c1412f825a080ac9ba4638c43da3163180b0f
aguo921 <aguo921@aucklanduni.ac.nz>
2023-03-30 16:46:33 +1300

    Initialise TextInput component

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,8 +1,10 @@
+import TextInput from "@/components/TextInput/TextInput";
 
 export default function HomePage() {
   return (
     <div>
       <p>Welcome to UABC Booking Portal.</p>
+      <TextInput />
     </div>
   )
 }
---
+++ packages/client/src/components/TextInput/TextInput.tsx
@@ -0,0 +1,5 @@
+const TextInput = () => {
+    return <div>Test</div>
+}
+
+export default TextInput
