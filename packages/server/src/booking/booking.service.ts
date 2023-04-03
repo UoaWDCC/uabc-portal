2b9ee3675832621dbcec9ff2b763fa9fc80220c6
Peter An <75343007+Exotica0122@users.noreply.github.com>
2023-04-03 14:55:05 +1200

    refund service

--- packages/server/src/booking/booking.service.ts
+++ packages/server/src/booking/booking.service.ts
@@ -1,23 +1,43 @@
 import { Types } from "mongoose";
 import { Booking, BookingModel } from "./booking";
+import { HttpError } from "../utils/http-error";
 
 export class BookingService {
-    public async refund(id: number) {
-        // mock return
-        const booking = await BookingModel.findOne();
-        if (booking) {
-            return booking;
+    public async refund(id: string) {
+        let foundBooking;
+
+        try {
+            foundBooking = await BookingModel.findById(id);
+        } catch (err) {
+            return new HttpError(
+                "Finding service by id failed, please try again",
+                500
+            );
         }
-        return {
-            userId: new Types.ObjectId(id),
-            sessionId: new Types.ObjectId(),
-            createdAt: Date.now(),
-            paymentMethod: new Types.ObjectId(),
-            status: "Pending",
-        };
+
+        if (!foundBooking) {
+            // write better status code
+            return new HttpError(
+                `Could not find the booking with the id ${id}.`,
+                404
+            );
+        }
+
+        foundBooking.status = "Refunded";
+
+        try {
+            foundBooking.save();
+        } catch (err) {
+            return new HttpError(
+                "Saving booking failed, please try again.",
+                500
+            );
+        }
+
+        return foundBooking;
     }
 
-    public cancel(id: number): Booking {
+    public cancel(id: string): Booking {
         // mock return
         return {
             userId: new Types.ObjectId(id),
