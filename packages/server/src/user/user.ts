df455c01945dd7f585d2bb976dcbe5c02e77fa07
tenmore <AdityaTendulkar@protonmail.com>
2023-04-07 00:01:10 +1200

    Added initial object

--- packages/server/src/user/user.ts
+++ packages/server/src/user/user.ts
@@ -1,7 +1,20 @@
-export interface User {
-    id: number;
-    email: string;
-    name: string;
-    status?: "Happy" | "Sad";
-    phoneNumbers: string[];
-  }
+import mongoose, { InferSchemaType, Schema } from "mongoose";
+
+const userSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, required: true},
+    firstName: { type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+
+    university: University,
+    universityID: UniversityID,
+    
+    isMember: Boolean,
+    sessionsReamining: Number,
+
+    previousDifficulty: Difficulty
+  });
+
+  export type User = InferSchemaType<typeof userSchema>;
+
+  export const UserModel = mongoose.model("User", userSchema);
