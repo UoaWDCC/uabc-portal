dd4e9bc526a184ca7c3ce45c3c3c066536758fea
tenmore <AdityaTendulkar@protonmail.com>
2023-04-07 13:05:59 +1200

    Added template for class functions

--- packages/server/src/user/user.service.ts
+++ packages/server/src/user/user.service.ts
@@ -1,24 +1,15 @@
-import { User } from "./user";
+import { Types } from "mongoose";
+import { User, UserModel } from "./user";
 
 // A post request should not contain an id.
-export type UserCreationParams = Pick<User, "email" | "name" | "phoneNumbers">;
-
 export class UsersService {
-  public get(id: number, name?: string): User {
-    return {
-      id,
-      email: "jane@doe.com",
-      name: name ?? "Jane Doe",
-      status: "Happy",
-      phoneNumbers: [],
-    };
+  public getFutureSessions() {
+    //function to go here
+    return;
   }
 
-  public create(userCreationParams: UserCreationParams): User {
-    return {
-      id: Math.floor(Math.random() * 10000), // Random
-      status: "Happy",
-      ...userCreationParams,
-    };
+  public getPastSessions() {
+    //function to go here
+    return;
   }
 }
