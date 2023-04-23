e3696f2f57d02c966672dc9931db8d3aaa2e73d3
lia-arroyo <liayzabel@gmail.com>
2023-04-23 15:26:30 +1200

    add tailwind css styling to reusable button component

--- packages/client/src/components/Button/Button.tsx
+++ packages/client/src/components/Button/Button.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Button = ({ label, onClick }: ButtonInputProps) => {
-  return <button onClick={onClick}>{label}</button>;
+  return (
+    <button
+      className="w-72 h-14 bg-blue-600 rounded text-white font-semibold"
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default Button;
