a472e328f28344d3d98326907529847f3b0e608e
aguo921 <aguo921@aucklanduni.ac.nz>
2023-04-27 16:39:10 +1200

    Author file

--- packages/client/src/components/TextInput/TextInput.tsx
+++ packages/client/src/components/TextInput/TextInput.tsx
@@ -1,3 +1,5 @@
+// Author: aguo921 (Angela Guo)
+
 import TextInputProps from "./TextInputProps"
 import { useState } from "react";
 
@@ -11,9 +13,10 @@ const TextInput = (props: TextInputProps) => {
             <label
                 htmlFor="input"
                 className={
-                    active || props.value != "" ?
-                    `absolute top-[-0.75rem] left-3 bg-white text-sm px-2 text-${borderColour} transition-all` :
-                    "absolute left-3 top-1.5 text-gray-500 cursor-text transition-all"
+                    `absolute left-3 transition-all
+                    ${active || props.value != "" ?
+                    `top-[-0.75rem] bg-white text-sm px-2 text-${borderColour}` : 
+                    `top-1.5 text-gray-500 cursor-text`}`
                 }
             >
                 {props.label}
