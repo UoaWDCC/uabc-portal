43c1f1ed0f4cd2243ced6fca17f8512a6e11830d
aguo921 <aguo921@aucklanduni.ac.nz>
2023-04-28 16:34:04 +1200

    Create payment options page

---
+++ packages/client/src/app/payment/page.tsx
@@ -0,0 +1,25 @@
+// Author: aguo921 (Angela Guo)
+
+"use client";
+
+import Heading from "./components/Heading/Heading";
+import PaymentInfoCard from "./components/PaymentInfoCard/PaymentInfoCard";
+import PaymentOptionCard from "./components/PaymentOptionCard/PaymentOptionCard";
+
+export default function PaymentOptionsPage() {
+  return (
+    <div>
+        <Heading>Payment</Heading>
+        <div className="p-2">
+          <PaymentInfoCard amount={15} />
+
+          {/* TODO: Make this div go to the bottom */}
+          <div>
+            <p className="font-medium text-center">Please select a payment method:</p>
+            <PaymentOptionCard onClick={() => alert('Direct debit')} title="Direct Debit" subtitle="Pay via Bank Transfer"/>
+            <PaymentOptionCard onClick={() => alert('Pay now')} title="Pay Now" subtitle="Pay via a Debit or Credit Card"/>
+          </div>
+        </div>
+    </div>
+  )
+}
