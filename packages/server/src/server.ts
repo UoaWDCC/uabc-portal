f68012e4e99cb787d9abe997197a726b2a5accd5
Michael Howell <mh.michaelhowell@gmail.com>
2023-04-29 14:27:01 +1200

    Revert "uabc-portal#34 | Creating booking model (#40)" (#48)
    This reverts commit 548d14d924dae0fac9e86328953f663bcaf434f7.

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,15 +1,7 @@
 import { app } from "./app";
-import { connectDB } from "./config/db";
-import { Booking } from "./booking/booking";
 
-const port = process.env.PORT || 3001;
-
-connectDB();
-
-app.get("/", (req, res, next) => {
-    res.status(200).json({ msg: "Hello World!" });
-});
+const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 );
