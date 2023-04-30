161d20e37d23475e7c43e90d601954a98f3ab59d
aguo921 <104069356+aguo921@users.noreply.github.com>
2023-04-30 16:45:56 +1200

    uabc-portal#7 | Create text input component (#47)
    * Initialise TextInput component
    
    * Implement text input functionality
    
    * Type TextInput props
    
    * Create test text input component
    
    * Basic border and label styles
    
    * Value field change
    
    * Minor style edit
    
    * Implement Tailwind CSS in TextInput component
    
    * Fixed issue of outline when text input is focused
    
    * Removed placeholder property
    
    * Change label location based on active state
    
    * Author file
    
    * Author file
    
    * Re-format author comment

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+
+import TextInput from "@/components/TextInput/TextInput";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [value, setValue] = useState("");
   return (
     <div>
       <p>Welcome to UABC Booking Portal.</p>
+      <TextInput
+        label="Label"
+        value={value}
+        onChange={setValue}
+      />
+      <p>{value}</p>
     </div>
   )
 }
