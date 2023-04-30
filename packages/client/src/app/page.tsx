8b623bb679313a7944c67281c1afe08ef4e976aa
Amisha <amishas171@gmail.com>
2023-04-30 15:23:29 +1200

    Implemented login page routing.

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <div>
-      <p>Welcome to UABC Booking Portal.</p>
+      <p>Welcome to UABC Booking Portal</p>
+      <p>
+        <Link href="/login">LOGIN</Link>
+      </p>
     </div>
-  )
+  );
 }
