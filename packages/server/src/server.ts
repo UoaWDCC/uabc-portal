a754ce4b89418be8cb5c9ceb00609002c5f91738
Michael Howell <mh.michaelhowell@gmail.com>
2023-05-01 18:32:10 +1200

    feat: update app init

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,10 +1,11 @@
 import { app } from "./app";
-import { connectDB } from "./mogno";
+import { connectDB } from "./mongo";
 
-connectDB();
+connectDB().then(() => {
+  const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000;
+  app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`)
+  );
+})
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
