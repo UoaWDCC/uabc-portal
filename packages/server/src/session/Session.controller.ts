5fa835785a6b5b665c68bfd0b6817b51ac9dfb68
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-05-03 21:31:44 +1200

    Implemented controller

--- packages/server/src/session/Session.controller.ts
+++ packages/server/src/session/Session.controller.ts
@@ -1,3 +1,47 @@
 /**
  * @author David Zhu <dzhu292@aucklanduni.ac.nz>
  */
+
+import {
+    Body,
+    Controller,
+    Get,
+    Path,
+    Post,
+    Patch,
+    Query,
+    Route,
+    SuccessResponse,
+  } from "tsoa";
+import { Session } from "./Session.model";
+import { SessionRepo } from "./Session.repo";
+  
+@Route("sessions")
+export class SessionController extends Controller {
+    @Get("{sessionId}")
+    public async getSession (
+        @Path() sessionId: string
+    ): Promise<Session|null> {
+        return await new SessionRepo().getById(sessionId);
+    }
+
+    @SuccessResponse("201", "Created") // Custom success response
+    @Post()
+    public async createSession (
+        @Body() request: Session
+    ) : Promise<void> {
+        this.setStatus(201);
+        await new SessionRepo().addSession(request);
+        return;
+    }
+
+    @SuccessResponse("201", "Updated") // Custom success response
+    @Patch()
+    public async updateSession (
+        @Body() request: Session
+    ) : Promise<void> {
+        this.setStatus(201);
+        await new SessionRepo().update(request.id, request);
+        return;
+    }
+}
