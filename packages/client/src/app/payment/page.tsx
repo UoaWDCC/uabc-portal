fa65b031e97d38acdd816fdc8b649cb64f0ffed1
aguo921 <aguo921@aucklanduni.ac.nz>
2023-05-04 14:57:23 +1200

    Edit layout of page

--- packages/client/src/app/payment/page.tsx
+++ packages/client/src/app/payment/page.tsx
@@ -4,24 +4,33 @@
 
 "use client";
 
-import Heading from "./components/Heading/Heading";
-import PaymentInfoCard from "./components/PaymentInfoCard/PaymentInfoCard";
-import PaymentOptionCard from "./components/PaymentOptionCard/PaymentOptionCard";
+import Heading from "../../components/Heading/Heading";
+import PaymentInfoCard from "../../components/PaymentInfoCard/PaymentInfoCard";
+import PaymentOptionCard from "../../components/PaymentOptionCard/PaymentOptionCard";
 
 export default function PaymentOptionsPage() {
   return (
     <div>
         <Heading>Payment</Heading>
+        
         <div className="p-2">
           <PaymentInfoCard amount={15} />
-
-          {/* TODO: Make this div go to the bottom */}
-          <div>
-            <p className="font-medium text-center">Please select a payment method:</p>
-            <PaymentOptionCard onClick={() => alert('Direct debit')} title="Direct Debit" subtitle="Pay via Bank Transfer"/>
-            <PaymentOptionCard onClick={() => alert('Pay now')} title="Pay Now" subtitle="Pay via a Debit or Credit Card"/>
-          </div>
         </div>
+
+        <div className="absolute bottom-5 left-0 w-full p-2">
+          <p className="font-medium text-center">Please select a payment method:</p>
+          <PaymentOptionCard
+            onClick={() => alert('Direct debit')}
+            title="Direct Debit"
+            subtitle="Pay via Bank Transfer"
+          />
+          <PaymentOptionCard
+            onClick={() => alert('Pay now')}
+            title="Pay Now"
+            subtitle="Pay via a Debit or Credit Card"
+          />
+        </div>
+
     </div>
   )
 }
