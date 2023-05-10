22e09d57fb2a34a2e3282746024b1a7a53e0911d
Peter An <75343007+Exotica0122@users.noreply.github.com>
2023-05-10 12:06:01 +1200

    move firebase config to env

--- packages/server/src/firebase.ts
+++ packages/server/src/firebase.ts
@@ -6,15 +6,30 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log(process.env.FIREBASE_API_KEY);
+
+if (!process.env.FIREBASE_API_KEY)
+    console.log('Please provide "FIREBASE_API_KEY" in .env');
+if (!process.env.FIREBASE_AUTH_DOMAIN)
+    console.log('Please provide "FIREBASE_AUTH_DOMAIN" in .env');
+if (!process.env.FIREBASE_PROJECT_ID)
+    console.log('Please provide "FIREBASE_PROJECT_ID" in .env');
+if (!process.env.FIREBASE_STORAGE_BUCKET)
+    console.log('Please provide "FIREBASE_STORAGE_BUCKET" in .env');
+if (!process.env.FIREBASE_MESSAGING_SENDER_ID)
+    console.log('Please provide "FIREBASE_MESSAGING_SENDER_ID" in .env');
+if (!process.env.FIREBASE_APP_ID)
+    console.log('Please provide "FIREBASE_APP_ID" in .env');
+if (!process.env.FIREBASE_MEASUREMENT_ID)
+    console.log('Please provide "FIREBASE_MEASUREMENT_ID" in .env');
+
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "uabc-portal.firebaseapp.com",
-    projectId: "uabc-portal",
-    storageBucket: "uabc-portal.appspot.com",
-    messagingSenderId: "15776391462",
-    appId: "1:15776391462:web:a06a04340cee318ec421e7",
-    measurementId: "G-M3JXX4YCP1",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
