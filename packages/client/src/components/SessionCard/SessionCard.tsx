14a6fbc7772769943aa4852cf235f9ea02be861a
Moeka <monjiro1025@gmail.com>
2023-05-11 14:54:02 +1200

    Fixed prop error with the checkmark

--- packages/client/src/components/SessionCard/SessionCard.tsx
+++ packages/client/src/components/SessionCard/SessionCard.tsx
@@ -13,7 +13,7 @@ function checkMark(status: SessionCardStatus) {
         <>
             {status == SessionCardStatus.SELECTED ?
                 <div className="absolute right-20">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-9 h-9">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" width="45" height="45">
                         <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
                     </svg>
                 </div>
