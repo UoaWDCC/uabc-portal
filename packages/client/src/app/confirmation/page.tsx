eb720f24bfcf32b00f01b08a7df57e5f9f758c5f
aguo921 <aguo921@aucklanduni.ac.nz>
2023-05-11 17:19:19 +1200

    Add confirmation banner and reformat Tailwind css

--- packages/client/src/app/confirmation/page.tsx
+++ packages/client/src/app/confirmation/page.tsx
@@ -6,34 +6,57 @@
 
 import ConfirmationIcon from "@/components/ConfirmationIcon/ConfirmationIcon";
 import ConfirmedSessionCard from "@/components/ConfirmedSessionCard/ConfirmedSessionCard";
+import ConfirmationBannerSVG from "public/images/ConfirmationBannerSVG.svg";
+
+const sessions = [
+  {
+    weekDay: "Wednesday",
+    locationName: "Auckland Badminton Association",
+    address: "99 Gillies Avenue, Epsom",
+    startTime: "5:00PM",
+    endTime: "7:00PM",
+  },
+  {
+    weekDay: "Friday",
+    locationName: "UoA Rec Center",
+    address: "7 Wynyard Street, Auckland City",
+    startTime: "7:30PM",
+    endTime: "10:00PM",
+  },
+];
 
 export default function ConfirmationPage() {
   return (
-    <div>
-        <div className="bg-blue-100 p-8" style={{borderRadius: '0 0 50% 50%'}}>
-            <p className="text-5xl font-bold text-center text-blue-600">UABC</p>
-        </div>
-        
+    <div className="flex flex-col h-[100dvh]">
+      <div className="relative">
+        <img src={ConfirmationBannerSVG.src} className="w-full" />
+        <p className="text-5xl font-bold text-center text-blue-600 z-10 absolute left-0 right-0 top-10">
+          UABC
+        </p>
+      </div>
 
+      <div className="grow flex justify-center items-center my-10 mx-20">
         <ConfirmationIcon confirmed={true} />
+      </div>
 
-        <div className="bg-blue-600 absolute bottom-0 left-0 w-full">
-            <p className="text-white mx-5 mt-5 text-xl font-medium">Rackets at the ready!</p>
-            <ConfirmedSessionCard
-                weekDay="Wednesday"
-                locationName="Auckland Badminton Association"
-                address="99 Gillies Avenue, Epsom"
-                startTime="5:00PM"
-                endTime="7:00PM"
-            />
-            <ConfirmedSessionCard
-                weekDay="Friday"
-                locationName="UoA Rec Center"
-                address="7 Wynyard Street, Auckland City"
-                startTime="7:30PM"
-                endTime="10:00PM"
-            />
+      <div className="bg-blue-600 overflow-hidden flex flex-col">
+        <p className="text-white mx-6 mt-5 mb-3 text-xl font-medium">
+          Rackets at the ready!
+        </p>
+        <div className="overflow-y-scroll max-h-80 flex gap-4 flex-col px-6 pb-4">
+          {sessions.map((session) => {
+            return (
+              <ConfirmedSessionCard
+                weekDay={session.weekDay}
+                locationName={session.locationName}
+                address={session.address}
+                startTime={session.startTime}
+                endTime={session.endTime}
+              />
+            );
+          })}
         </div>
+      </div>
     </div>
-  )
+  );
 }
