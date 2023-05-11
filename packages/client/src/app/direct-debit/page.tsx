2cd7d5000b44ff51c5c5ba7bd228ff2cf9eadd36
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-05-11 15:52:32 +1200

    Fixed growing flexbox debit page

--- packages/client/src/app/direct-debit/page.tsx
+++ packages/client/src/app/direct-debit/page.tsx
@@ -9,43 +9,39 @@ import PaymentInfoCard from "@/components/PaymentInfoCard/PaymentInfoCard";
 import DebitDetailsCard from "@/components/DebitDetailsCard/DebitDetailsCard";
 import Button from "@/components/Button/Button";
 
-export default function DirectDebitPage () {
-    const firstName:string = "John"
-    const lastName:string = "Smith"
-    const sessionId:string = "rn3498"
-    const accountNumber:string = "xx-xxxx-xxxx-xxx"
+export default function DirectDebitPage() {
+  const firstName: string = "John";
+  const lastName: string = "Smith";
+  const sessionId: string = "rn3498";
+  const accountNumber: string = "xx-xxxx-xxxx-xxx";
 
-    return (
-        <div className="h-[90vh] flex flex-col p-2"> {/* 90vh accounts for address bar on ios and etc */}
-
-                <Heading>Direct Debit</Heading>
-                <PaymentInfoCard amount={15} />
-
-            <div className="pb-7 mt-auto">
-                <p className="font-medium text-center">Direct Debit</p>
-                <DebitDetailsCard
-                    title="Account Number:"
-                    text={[accountNumber]}
-                    copy={true}
-                    onClick={() => {
-                        navigator.clipboard.writeText(accountNumber)
-                    }}
-                />
-                <DebitDetailsCard
-                    title="Reference:"
-                    text={[firstName+" "+lastName]}
-                    sessionId={sessionId}
-                    copy={true}
-                    onClick={() => {navigator.clipboard.writeText(sessionId)}}
-                />
-
-                <div className="flex justify-center">
-                    <Button
-                        label="done" 
-                        onClick={() => alert('DONE')}
-                        />
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="h-[100dvh] flex flex-col p-2">
+      <Heading>Direct Debit</Heading>
+      <PaymentInfoCard amount={15} />
+      <div className="pb-7 mt-auto grow flex flex-col-reverse">
+        <DebitDetailsCard
+          title="Reference:"
+          text={[firstName + " " + lastName]}
+          sessionId={sessionId}
+          copy={true}
+          onClick={() => {
+            navigator.clipboard.writeText(sessionId);
+          }}
+        />
+        <DebitDetailsCard
+          title="Account Number:"
+          text={[accountNumber]}
+          copy={true}
+          onClick={() => {
+            navigator.clipboard.writeText(accountNumber);
+          }}
+        />
+        <p className="font-medium text-center">Direct Debit</p>
+      </div>
+      <div className="flex justify-center">
+        <Button label="done" onClick={() => alert("DONE")} />
+      </div>
+    </div>
+  );
 }
--- packages/client/src/components/DebitDetailsCard/DebitDetailsCard.tsx
+++ packages/client/src/components/DebitDetailsCard/DebitDetailsCard.tsx
@@ -2,32 +2,41 @@
  * @author David Zhu <dzhu292@aucklanduni.ac.nz>
  */
 
-import Card from '../Card/Card';
-import DebitDetailsCardProps from './DebitDetailsCardProps';
-import { MdContentCopy } from 'react-icons/md';
-
+import Card from "../Card/Card";
+import DebitDetailsCardProps from "./DebitDetailsCardProps";
+import { MdContentCopy } from "react-icons/md";
 
 const DebitDetailsCard = (props: DebitDetailsCardProps) => {
-    const body = props.text.map((text, index) => 
-        <p className="font-normal text-gray-500" key={index}>{text}</p>); // TODO: redundant
-    if (typeof(props.sessionId) != "undefined") {
-        body.push(<br/>,
-        <p className="font-normal text-gray-500 inline">SessionID: </p>, // TODO: try not to use this
-        <p className="font-bold text-gray-500 inline">{props.sessionId}</p>)
-    }
-    if (typeof(props.copy) != "undefined") { // dont need bg-change for mobile devices but can be used for desktop
-        body.push(
-            <button onPointerDown={props.onClick} className="absolute right-4 top-1/2 translate-y-[-50%] p-2 rounded-lg active:bg-gray-300">
-                <MdContentCopy className="text-3xl"/>
-            </button>
-        )
-    }
-    return (
-        <Card className="bg-gray-200 p-5 pt-50 relative">
-            <p className="font-medium text-xl top-5">{props.title}</p>
-            {body}
-        </Card>
-    )
-}
+  return (
+    <Card className="bg-gray-200 p-5 pt-50 relative">
+      <p className="font-medium text-xl top-5">{props.title}</p>
 
-export default DebitDetailsCard
+      {props.text.map((text, index) => {
+        return (
+          <p className="font-normal text-gray-500" key={index}>
+            {text}
+          </p>
+        );
+      })}
+
+      {props.sessionId && (
+        <>
+          <br />
+          <p className="font-normal text-gray-500 inline">SessionID: </p>
+          <p className="font-bold text-gray-500 inline">{props.sessionId}</p>
+        </>
+      )}
+
+      {props.copy && (
+        <button
+          onPointerDown={props.onClick}
+          className="absolute right-4 top-1/2 translate-y-[-50%] p-2 rounded-lg active:bg-gray-300"
+        >
+          <MdContentCopy className="text-3xl" />
+        </button>
+      )}
+    </Card>
+  );
+};
+
+export default DebitDetailsCard;
