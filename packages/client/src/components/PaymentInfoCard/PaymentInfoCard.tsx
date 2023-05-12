ec9c0c885d80fd831799ca7c5284280a1187c8c8
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-05-12 23:26:23 +1200

    Changed style figma design

--- packages/client/src/components/PaymentInfoCard/PaymentInfoCard.tsx
+++ packages/client/src/components/PaymentInfoCard/PaymentInfoCard.tsx
@@ -9,8 +9,8 @@ const PaymentInfoCard = (props: PaymentInfoCardProps) => {
   const dollarAmount = "$" + (Math.round(props.amount * 100) / 100).toFixed(2);
 
   return (
-    <Card className="text-center p-8 pt-12 bg-[#3767af] text-white">
-      <p className="text-blue-100 text-xs">Your total for this session:</p>
+    <Card className="text-center p-8 pt-12 bg-[#3767af] text-white shadow-xl">
+      <p className="text-[#AFCFFF] text-xs">Your total for this session:</p>
       <p className="text-3xl font-bold mb-5">{dollarAmount}</p>
       <p className="text-xs">Casual Badminton Session</p>
     </Card>
