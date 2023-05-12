5bd32621930cdda3d9ffc4222ea11d7535da1801
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-05-12 15:50:17 +1200

    Changed padding

--- packages/client/src/app/direct-debit/page.tsx
+++ packages/client/src/app/direct-debit/page.tsx
@@ -22,7 +22,7 @@ export default function DirectDebitPage() {
       <p className="mt-auto font-medium text-center flex flex-col-reverse grow pt-3">
         Direct Debit
       </p>
-      <div className="flex flex-col overflow-y-auto w-full gap-4 bg-bottom py-3 scroll-fade">
+      <div className="flex flex-col overflow-y-auto w-full gap-4 bg-bottom py-4 scroll-fade">
         <DebitDetailsCard
           title="Account Number:"
           text={[accountNumber]}
