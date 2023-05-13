0119fb3a46a7d3379dd1a8f322a1cb34306557eb
aguo921 <aguo921@aucklanduni.ac.nz>
2023-05-13 23:33:50 +1200

    Test level selector component

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 
+import LevelSelector from "@/components/LevelSelector/LevelSelector";
 import TextInput from "@/components/TextInput/TextInput";
 import { useState } from "react";
 
 export default function HomePage() {
   const [value, setValue] = useState("");
+  const [open, setOpen] = useState(false);
+  const [level, setLevel] = useState("Intermediate");
+
   return (
     <div>
       <p>Welcome to UABC Booking Portal.</p>
       <TextInput label="Label" value={value} onChange={setValue} />
-      <p>{value}</p>
+      <p>Text input value: {value}</p>
+      <button onClick={() => setOpen(true)}>Open level selector</button>
+      <LevelSelector
+        open={open}
+        onClose={() => setOpen(false)}
+        level={level}
+        onSelect={setLevel}
+      />
+      <p>Level: {level}</p>
     </div>
   );
 }
