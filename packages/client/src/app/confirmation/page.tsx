eaec5e6de9700881855dbe817f22216101ea7fe9
aguo921 <104069356+aguo921@users.noreply.github.com>
2023-06-06 21:55:16 +1200

    uabc-portal#44 | Create confirmation screen (#69)
    * Initialise TextInput component
    
    * Implement text input functionality
    
    * Type TextInput props
    
    * Create test text input component
    
    * Basic border and label styles
    
    * Value field change
    
    * Minor style edit
    
    * Implement Tailwind CSS in TextInput component
    
    * Fixed issue of outline when text input is focused
    
    * Removed placeholder property
    
    * Change label location based on active state
    
    * Author file
    
    * Author file
    
    * Create PaymentOptionsPage components
    
    * Create payment options page
    
    * Install react-icons
    
    * Re-format author comment
    
    * Move components into another folder
    
    * Edit layout of page
    
    * Create confirmation icon component
    
    * Create session info card
    
    * Create confirmation page
    
    * Round bottom of header
    
    * Change component name to ConfirmedSessionCard
    
    * Remove margins from components
    
    * Add confirmation banner and reformat Tailwind css
    
    * Fix child key warning
    
    * Add settings
    
    ---------
    
    Co-authored-by: Michael Howell <mh.michaelhowell@gmail.com>

---
+++ packages/client/src/app/confirmation/page.tsx
@@ -0,0 +1,67 @@
+/**
+ * @author Angela Guo <aguo921@aucklanduni.ac.nz>
+ */
+
+"use client";
+
+import ConfirmationIcon from "@/components/ConfirmationIcon/ConfirmationIcon";
+import ConfirmedSessionCard from "@/components/ConfirmedSessionCard/ConfirmedSessionCard";
+import ConfirmationBannerSVG from "public/images/ConfirmationBannerSVG.svg";
+
+const sessions = [
+  {
+    id: 0,
+    weekDay: "Wednesday",
+    locationName: "Auckland Badminton Association",
+    address: "99 Gillies Avenue, Epsom",
+    startTime: "5:00PM",
+    endTime: "7:00PM",
+  },
+  {
+    id: 1,
+    weekDay: "Friday",
+    locationName: "UoA Rec Center",
+    address: "7 Wynyard Street, Auckland City",
+    startTime: "7:30PM",
+    endTime: "10:00PM",
+  },
+];
+
+const confirmed = true;
+
+export default function ConfirmationPage() {
+  return (
+    <div className="flex flex-col h-[100dvh]">
+      <div className="relative">
+        <img src={ConfirmationBannerSVG.src} className="w-full" />
+        <p className="text-5xl font-bold text-center text-blue-600 z-10 absolute left-0 right-0 top-10">
+          UABC
+        </p>
+      </div>
+
+      <div className="grow flex justify-center items-center my-10 mx-20">
+        <ConfirmationIcon confirmed={confirmed} />
+      </div>
+
+      <div className="bg-blue-600 overflow-hidden flex flex-col">
+        <p className="text-white mx-6 mt-5 mb-3 text-xl font-medium">
+          Rackets at the ready!
+        </p>
+        <div className="overflow-y-scroll max-h-80 flex gap-4 flex-col px-6 pb-4">
+          {sessions.map((session) => {
+            return (
+              <ConfirmedSessionCard
+                key={session.id}
+                weekDay={session.weekDay}
+                locationName={session.locationName}
+                address={session.address}
+                startTime={session.startTime}
+                endTime={session.endTime}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
