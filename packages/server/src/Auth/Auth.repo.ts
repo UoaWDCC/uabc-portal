52bfb9f9d9d4b094694793cc1e47a7b9e65db103
Peter An <75343007+Exotica0122@users.noreply.github.com>
2023-06-06 22:20:24 +1200

    #24 - Integrate firebase auth (#59)
    * janky firebase auth init
    
    * login and swagger docs
    
    * rename body params
    
    * move firebase config to env
    
    * remove unnecessary comments
    
    ---------
    
    Co-authored-by: Michael Howell <mh.michaelhowell@gmail.com>

---
+++ packages/server/src/Auth/Auth.repo.ts
@@ -0,0 +1,54 @@
+/***
+ * @author Peter An <anpeteran2@gmail.com>
+ */
+
+import {
+    Auth,
+    createUserWithEmailAndPassword,
+    getAuth,
+    signInWithEmailAndPassword,
+} from "firebase/auth";
+import { app } from "../firebase";
+
+export class AuthRepo {
+    private auth: Auth;
+    constructor() {
+        this.auth = getAuth(app);
+    }
+
+    /**
+     * Log In user with email password
+     */
+    async login(email: string, password: string) {
+        return signInWithEmailAndPassword(this.auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                return user;
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                return { code: errorCode, message: errorMessage };
+            });
+    }
+
+    /**
+     * Sign Up user with email password
+     */
+    async signUp(email: string, password: string) {
+        return createUserWithEmailAndPassword(this.auth, email, password)
+            .then((userCredential) => {
+                // Sign Up
+                const user = userCredential.user;
+                console.log(user);
+                return user;
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                return { code: errorCode, message: errorMessage };
+            });
+    }
+}
---
+++ packages/server/src/firebase.ts
@@ -0,0 +1,33 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+if (!process.env.FIREBASE_API_KEY)
+    console.log('Please provide "FIREBASE_API_KEY" in .env');
+if (!process.env.FIREBASE_AUTH_DOMAIN)
+    console.log('Please provide "FIREBASE_AUTH_DOMAIN" in .env');
+if (!process.env.FIREBASE_PROJECT_ID)
+    console.log('Please provide "FIREBASE_PROJECT_ID" in .env');
+if (!process.env.FIREBASE_STORAGE_BUCKET)
+    console.log('Please provide "FIREBASE_STORAGE_BUCKET" in .env');
+if (!process.env.FIREBASE_MESSAGING_SENDER_ID)
+    console.log('Please provide "FIREBASE_MESSAGING_SENDER_ID" in .env');
+if (!process.env.FIREBASE_APP_ID)
+    console.log('Please provide "FIREBASE_APP_ID" in .env');
+if (!process.env.FIREBASE_MEASUREMENT_ID)
+    console.log('Please provide "FIREBASE_MEASUREMENT_ID" in .env');
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = isSupported().then((yes) =>
+    yes ? getAnalytics(app) : null
+);
