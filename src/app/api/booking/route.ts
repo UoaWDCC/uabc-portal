493bf004042d98785a9d865a6f00c08f86b16735
Exotica0122 <anpeteran2@gmail.com>
2023-08-16 18:52:39 +1200

    refactor todos for searchParams

--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -1,8 +1,33 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/db";
 
 // @TODO remove this later
 import { ObjectId } from "bson";
+
+/**
+ * Get booking by id
+ */
+export async function GET(request: NextRequest) {
+  const bookingId = request.nextUrl.searchParams.get("id");
+
+  if (!bookingId) {
+    return NextResponse.json(
+      { data: {}, msg: "No id provided in the request" },
+      { status: 404 },
+    );
+  }
+
+  const booking = await prisma.booking.findFirst({ where: { id: bookingId } });
+
+  if (!booking) {
+    return NextResponse.json(
+      { data: {}, msg: `no booking found for id: ${bookingId}` },
+      { status: 404 },
+    );
+  }
+
+  return NextResponse.json({ data: booking });
+}
 
 /**
  * Create a new booking
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -5,13 +5,22 @@ import { prisma } from "@/db";
  * Get payment by id
  */
 export async function GET(request: NextRequest) {
-  // @TODO Janky solution please fix in future
-  const userId = request.nextUrl.pathname.split("/")[3];
+  const userId = request.nextUrl.searchParams.get("id");
+
+  if (!userId) {
+    return NextResponse.json(
+      { data: {}, msg: "No id provided in the request" },
+      { status: 404 },
+    );
+  }
 
   const user = await prisma.user.findFirst({ where: { id: userId } });
 
   if (!user) {
-    return NextResponse.json({ data: {}, msg: "error" }, { status: 404 });
+    return NextResponse.json(
+      { data: {}, msg: `no payment found for id: ${userId}` },
+      { status: 404 },
+    );
   }
 
   return NextResponse.json({ data: user, msg: "success" });
