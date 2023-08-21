b8dd4583473520060fa9f605d75f0c8b08832b06
aguo921 <aguo921@aucklanduni.ac.nz>
2023-08-21 16:23:36 +1200

    Update session input fields

--- src/components/SessionInputForm/SessionInputForm.tsx
+++ src/components/SessionInputForm/SessionInputForm.tsx
@@ -9,49 +9,102 @@ import DatePicker from "react-datepicker";
 import Button from "../Button/Button";
 import "react-datepicker/dist/react-datepicker.css";
 import Card from "../Card/Card";
+import NumberInput from "../NumberInput/NumberInput";
 
 // TODO: Database connection
 
-const SessionInputForm = (props: SessionInputFormProps) => {
-    const [location, setLocation] = useState("");
-    const [startDate, setStartDate] = useState(new Date())
-    const [endDate, setEndDate] = useState(new Date());
+// model GameSession {
+//     id           String   @id @default(auto()) @map("_id") @db.ObjectId
+//     bookingClose DateTime
+//     bookingOpen  DateTime
+//     startTime    DateTime
+//     endTime      DateTime
+//     location     String
+//     maxUsers     Int
+//   }
 
-    const handleStartDateChange = (date: Date | null) => {
+const SessionInputForm = (props: SessionInputFormProps) => {
+    const [bookingClose, setBookingClose] = useState(new Date());
+    const [bookingOpen, setBookingOpen] = useState(new Date());
+    const [startTime, setStartTime] = useState(new Date());
+    const [endTime, setEndTime] = useState(new Date());
+    const [location, setLocation] = useState("");
+    const [maxUsers, setMaxUsers] = useState(0);
+
+    const handleStartTimeChange = (date: Date | null) => {
         if (date != null) {
-            setStartDate(date);
+            setStartTime(date);
         }
     }
 
-    const handleEndDateChange = (date: Date | null) => {
+    const handleEndTimeChange = (date: Date | null) => {
         if (date != null) {
-            setEndDate(date);
+            setEndTime(date);
+        }
+    }
+
+    
+    const handleBookingCloseChange = (date: Date | null) => {
+        if (date != null) {
+            setBookingClose(date);
+        }
+    }
+
+    const handleBookingOpenChange = (date: Date | null) => {
+        if (date != null) {
+            setBookingOpen(date);
         }
     }
 
     const onSubmit = () => {
-        alert(location + "\n" + startDate + "\n" + endDate);
+        alert(location + "\n" + startTime + "\n" + endTime + "\n" + bookingOpen + "\n" + bookingClose + "\n" + maxUsers);
+    }
+
+    const handleMaxUsersChange = (value: string) => {
+        // TODO: validation
+        setMaxUsers(Number(value))
     }
 
     return (
         <Card>
             <div className="my-5">
-                <TextInput label="Location" value={location} onChange={setLocation}/>
+                <TextInput label="Location" value={location} onChange={setLocation} type="text" isError={false}/>
             </div>
             <div className="my-5">
-                <p>Start Date</p>
+                <TextInput label="Maximum Users" value={maxUsers.toString()} onChange={handleMaxUsersChange} type="text" isError={false}/>
+            </div>
+            <div className="my-5">
+                <p>Start Time</p>
                 <DatePicker
-                    selected={startDate}
-                    onChange={handleStartDateChange}
+                    selected={startTime}
+                    onChange={handleStartTimeChange}
                     showTimeSelect
                     dateFormat="Pp"
                 />
             </div>
             <div className="my-5">
-                <p>End Date</p>
+                <p>End Time</p>
                 <DatePicker
-                    selected={endDate}
-                    onChange={handleEndDateChange}
+                    selected={endTime}
+                    onChange={handleEndTimeChange}
+                    showTimeSelect
+                    dateFormat="Pp"
+                />
+            </div>
+            <div className="my-5">
+                <p>Booking Open</p>
+                <DatePicker
+                    selected={bookingOpen}
+                    onChange={handleBookingOpenChange}
+                    showTimeSelect
+                    dateFormat="Pp"
+                />
+            </div>
+            <div className="my-5">
+                <p>Booking Close</p>
+                <DatePicker
+                    selected={bookingClose}
+                    onChange={handleBookingCloseChange}
                     showTimeSelect
                     dateFormat="Pp"
                 />
