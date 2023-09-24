a75a0d4fa453b3fae692f9489961edd2ad2cc277
lia-arroyo <liayzabel@gmail.com>
2023-09-25 12:23:32 +1300

    add session, booking and user details in the email (#30)

--- src/app/api/booking/send-email/route.ts
+++ src/app/api/booking/send-email/route.ts
@@ -1,4 +1,6 @@
 /**
+ * This endpoint sends an email with the booking details to the user.
+ * To use, send a @type SendEmailRequest with the booking ID in the POST request body.
  * @author Lia Arroyo <liayzabel@gmail.com>
  */
 
@@ -27,18 +29,41 @@ export async function POST(request: NextRequest) {
     where: { id: booking.sessionId },
   })) as GameSession;
 
-  // sending email using sendgrid
+  // setting up sendgrid
   const sgMail = require("@sendgrid/mail");
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  const msg = {
-    to: "liayzabel@gmail.com",
-    from: "uabc@projects.wdcc.co.nz",
-    subject: "Sending with SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour12: true,
   };
 
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const msg = {
+    to: [user.email],
+    from: "uabc@projects.wdcc.co.nz",
+    subject: `UABC Booking Confirmed for ${user.name}`,
+    text: `Thank you, ${
+      user.name
+    }, for booking a session with UABC! Your booking is confirmed:\n
+    Location: ${gameSession.location}\n
+    Date: ${gameSession.startTime.toLocaleDateString(
+      "en-NZ",
+      dateOptions,
+    )} ${gameSession.startTime.toLocaleTimeString(
+      "en-NZ",
+      timeOptions,
+    )} - ${gameSession.endTime.toLocaleTimeString("en-NZ", timeOptions)}\n
+    Booking made on ${booking.createdAt}.
+    `,
+  };
+
+  // sending email using sendgrid
   sgMail
     .send(msg)
     .then(() => {
