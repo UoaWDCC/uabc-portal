046b9fc9c312f81fb63800f34395ad04b5ae9e62
lia-arroyo <liayzabel@gmail.com>
2023-09-25 11:45:54 +1300

    add sendgrid to send email

--- src/app/api/booking/send-email/route.ts
+++ src/app/api/booking/send-email/route.ts
@@ -2,12 +2,13 @@
  * @author Lia Arroyo <liayzabel@gmail.com>
  */
 
-type SendEmailRequest = {
+export type SendEmailRequest = {
   bookingId: string;
 };
 
 import { prisma } from "@/db";
 import { Booking, GameSession, User } from "@prisma/client";
+import { Exception } from "@prisma/client/runtime/library";
 import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -26,5 +27,24 @@ export async function POST(request: NextRequest) {
     where: { id: booking.sessionId },
   })) as GameSession;
 
-  // TODO: send email using SendGrid API
+  // sending email using sendgrid
+  const sgMail = require("@sendgrid/mail");
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const msg = {
+    to: "liayzabel@gmail.com",
+    from: "uabc@projects.wdcc.co.nz",
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error: Exception) => {
+      console.error(error);
+    });
 }
