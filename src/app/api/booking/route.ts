c4f6e6363574e5634efe6f74c555aa3cc1165545
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-10-09 02:35:47 +1300

    feat: added POST endpoint for booking from cart

--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -32,17 +32,98 @@ export async function GET(request: NextRequest) {
 /**
  * Create a new booking
  */
-export async function POST() {
+export async function POST(req: NextRequest) {
   // @TODO auth users
-  const newBooking = await prisma.booking.create({
-    data: {
-      createdAt: new Date(),
-      paymentMethod: new ObjectId().toString(),
-      status: "pending",
-      userId: new ObjectId().toString(),
-      sessionId: new ObjectId().toString(),
+  const { userId } = await req.json();
+
+  if (!userId) {
+    return NextResponse.json(
+      { data: {}, msg: "No userId provided in the request" },
+      { status: 404 },
+    );
+  }
+
+  const cart = await prisma.checkoutCart.findFirst({
+    where: {
+      userId,
     },
   });
 
-  return NextResponse.json({ data: newBooking });
+  if (!cart) {
+    return NextResponse.json(
+      { data: {}, msg: "cart not found" },
+      { status: 404 },
+    );
+  }
+
+  const cartItems = await prisma.checkoutCartItem.findMany({
+    where: {
+      checkoutCartId: cart.id,
+    },
+  });
+
+  if (!cartItems.length) {
+    return NextResponse.json(
+      { data: {}, msg: "no cart items found" },
+      { status: 404 },
+    );
+  }
+
+  const newBookings = [];
+
+  for (const cartItem of cartItems) {
+    const session = await prisma.gameSession.findFirst({
+      where: {
+        id: cartItem.gameSessionId,
+      },
+    });
+
+    if (!session) {
+      return NextResponse.json(
+        { data: {}, msg: "session not found" },
+        { status: 404 },
+      );
+    }
+    const bookingCount = await prisma.booking.count({
+      where: {
+        sessionId: cartItem.gameSessionId,
+      },
+    });
+
+    // TODO: decide what to do here, make one go through but not the other?
+    if (bookingCount >= session.maxUsers) {
+      return NextResponse.json(
+        { data: {}, msg: "session is full" },
+        { status: 404 },
+      );
+    }
+
+    const booking = await prisma.booking.findFirst({
+      where: {
+        sessionId: cartItem.gameSessionId,
+        userId: userId,
+      },
+    });
+
+    if (booking) {
+      return NextResponse.json(
+        { data: {}, msg: "session already booked" },
+        { status: 404 },
+      );
+    }
+
+    newBookings.push(
+      await prisma.booking.create({
+        data: {
+          createdAt: new Date(),
+          userId,
+          sessionId: cartItem.gameSessionId,
+          paymentMethod: new ObjectId().toString(),
+          status: "pending",
+        },
+      }),
+    );
+  }
+
+  return NextResponse.json({ data: newBookings });
 }
