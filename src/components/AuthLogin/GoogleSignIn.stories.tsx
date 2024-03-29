19875ef4cd4bdc9d708cf74cbcd06e29a9d6dd02
Tulza <40633228+tulza@users.noreply.github.com>
2024-03-30 12:16:30 +1300

    resolve storybook public image not importing to sb

---
+++ src/components/AuthLogin/GoogleSignIn.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import GoogleSignIn from "./GoogleSignIn";
+
+const meta = {
+  title: "GoogleSignInButton",
+  component: GoogleSignIn,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof GoogleSignIn>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
