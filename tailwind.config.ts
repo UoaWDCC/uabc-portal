35e2ccd1df340e0a9fbbc6a0cee4ab3d4ad197e6
PrabhjotSodhi <sodhiprabhjot23@gmail.com>
2024-03-30 17:50:29 +1300

    Added Inter & sans-serif as the default font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
