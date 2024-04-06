a087fe11035d72180705f6c676f9c137d7775d36
LiamParker1 <lpar161@aucklanduni.ac.nz>
2024-04-07 00:12:24 +1300

    Remove useState and make other code review changes

--- src/app/onboard/name/page.tsx
+++ src/app/onboard/name/page.tsx
@@ -1,51 +1,47 @@
 "use client";
 
-import { useState } from "react";
-
 import UabcHeaderText from "@/components/AuthLogin/UabcHeaderText";
 import { Button } from "@/components/Button";
 import { TextInput } from "@/components/TextInput";
 import { useAccountStore } from "@/stores/accountDetails";
 
 export default function NamePage() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const firstName = useAccountStore((state) => state.firstName);
+  const lastName = useAccountStore((state) => state.lastName);
 
   const updateFirstName = useAccountStore((state) => state.setFirstName);
   const updateLastName = useAccountStore((state) => state.setLastName);
 
   return (
-    <div className="h-[100dvh] w-[100dvw]">
+    <div className="h-dvh w-dvw">
       <div className="mx-4 flex flex-col h-full justify-between">
         <div className="pt-4">
           <UabcHeaderText />
         </div>
-        <div className="flex flex-col space-y-4">
+        <div className="flex flex-col gap-6">
           <p className="text-center">What&apos;s your name?</p>
           <TextInput
             label="First Name"
             value={firstName}
-            type="firstName"
+            type="text"
             isError={false}
-            onChange={setFirstName}
+            onChange={updateFirstName}
           />
           <TextInput
             label="Last Name"
             value={lastName}
-            type="lastName"
+            type="text"
             isError={false}
-            onChange={setLastName}
+            onChange={updateLastName}
           />
         </div>
         <div className="pb-10">
           <Button
             className="w-full"
             onClick={() => {
-              updateFirstName(firstName);
-              updateLastName(lastName);
               // Go to membership type selection page
             }}
-            disabled={firstName == "" || lastName == "" ? true : false}
+            disabled={firstName === "" || lastName === "" ? true : false}
           >
             Continue
           </Button>
