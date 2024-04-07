d3de4515dcbe37661535d4ccb3b06e01820873f0
Tulza <40633228+tulza@users.noreply.github.com>
2024-04-08 01:29:42 +1200

    resolve pr conversation

--- src/components/AuthLogin/AuthLoginMobile.tsx
+++ src/components/AuthLogin/AuthLoginMobile.tsx
@@ -7,21 +7,21 @@ import GoogleSignIn from "./GoogleSignIn";
 
 const AuthLoginMobile = () => {
   return (
-    <div className="h-[100dvh] w-[100dvw] bg-[#0A172A] grid place-items-center">
-      <div className="w-[320px] flex-col h-full flex items-center justify-center">
+    <div className="h-dvh w-dvw bg-[#0A172A] grid place-items-center">
+      <div className="w-dvw px-4 flex-col h-full flex items-center justify-center">
         <Image src="/svgs/logo2.svg" alt="uabc logo" width={225} height={225} />
         <div className="mt-6 flex flex-col w-full">
           <p className="text-white text-center text-base">
             Login into your account
           </p>
           <EmailLogin className="mt-4 text-sm" />
-          <p className="text-center text-white text-sm">
-            Don&apos;t have an account?{" "}
-            <span className="underline font-bold">Sign up</span>
-          </p>
         </div>
         <BreakLine label="or" />
         <GoogleSignIn className="w-full p-6 h-12" />
+        <p className="text-center text-white text-xs">
+          Don&apos;t have an account?{" "}
+          <span className="underline font-bold">Sign up</span>
+        </p>
       </div>
     </div>
   );
--- src/components/AuthLogin/BreakLine.tsx
+++ src/components/AuthLogin/BreakLine.tsx
@@ -4,7 +4,7 @@ const BreakLine = ({ label }: { label: string }) => {
   return (
     <div className="mt-2 mb-4 flex whitespace-nowrap w-full justify-center items-center">
       <hr className="w-full border-tertiary" />
-      <label className="m-2 w-min rounded-lg text-white text-sm">{label}</label>
+      <span className="m-2 w-min rounded-lg text-white text-sm">{label}</span>
       <hr className="w-full border-tertiary" />
     </div>
   );
