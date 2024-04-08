578399929f19a0de1fb8f97c0eb29bf146380e85
Tulza <40633228+tulza@users.noreply.github.com>
2024-04-08 15:18:54 +1200

    breakline border size to 0.5

--- src/components/AuthLogin/BreakLine.tsx
+++ src/components/AuthLogin/BreakLine.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const BreakLine = ({ label }: { label: string }) => {
   return (
     <div className="flex whitespace-nowrap w-full justify-center items-center">
-      <hr className="w-full border-white" />
+      <hr className="w-full border-white border-0.5" />
       <span className="m-2 w-min h-4 rounded-lg text-white text-sm uppercase">
         {label}
       </span>
-      <hr className="w-full border-white" />
+      <hr className="w-full border-white border-0.5" />
     </div>
   );
 };
