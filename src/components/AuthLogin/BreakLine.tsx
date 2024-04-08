026e2197a8722b2d8b2fed25cbd7bde618562ac2
Tulza <40633228+tulza@users.noreply.github.com>
2024-04-08 15:15:05 +1200

    fixed breakline styling to figma

--- src/components/AuthLogin/BreakLine.tsx
+++ src/components/AuthLogin/BreakLine.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const BreakLine = ({ label }: { label: string }) => {
   return (
     <div className="flex whitespace-nowrap w-full justify-center items-center">
-      <hr className="w-full border-tertiary" />
-      <span className="m-2 w-min rounded-lg text-white text-sm uppercase">
+      <hr className="w-full border-white" />
+      <span className="m-2 w-min h-4 rounded-lg text-white text-sm uppercase">
         {label}
       </span>
-      <hr className="w-full border-tertiary" />
+      <hr className="w-full border-white" />
     </div>
   );
 };
