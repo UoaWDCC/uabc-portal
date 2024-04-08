6ad4df4a2f107ff4e47ce02aede04d45fa2c6971
Tulza <40633228+tulza@users.noreply.github.com>
2024-04-08 15:06:41 +1200

    fixing spacing to match figma

--- src/components/AuthLogin/AuthLoginMobile.tsx
+++ src/components/AuthLogin/AuthLoginMobile.tsx
@@ -4,24 +4,23 @@ import Image from "next/image";
 import BreakLine from "./BreakLine";
 import EmailLogin from "./EmailLogin";
 import GoogleSignIn from "./GoogleSignIn";
+import UabcHeaderText from "./UabcHeaderText";
 
 const AuthLoginMobile = () => {
   return (
     <div className="h-dvh w-dvw bg-[#0A172A] grid place-items-center">
-      <div className="w-dvw px-4 flex-col h-full flex items-center justify-center">
+      <div className="w-dvw px-4 flex-col h-full flex items-center justify-between py-8">
+        <UabcHeaderText />
         <Image src="/svgs/logo2.svg" alt="uabc logo" width={225} height={225} />
-        <div className="mt-6 flex flex-col w-full">
-          <p className="text-white text-center text-base">
-            Login into your account
+        <div className="mt-6 flex flex-col w-full gap-4">
+          <EmailLogin className="text-sm" />
+          <BreakLine label="or" />
+          <GoogleSignIn className="w-full p-6 h-12" />
+          <p className="text-center text-white text-xs mt-6">
+            Don&apos;t have an account?{" "}
+            <span className="underline font-bold">Sign up</span>
           </p>
-          <EmailLogin className="mt-4 text-sm" />
         </div>
-        <BreakLine label="or" />
-        <GoogleSignIn className="w-full p-6 h-12" />
-        <p className="text-center text-white text-xs">
-          Don&apos;t have an account?{" "}
-          <span className="underline font-bold">Sign up</span>
-        </p>
       </div>
     </div>
   );
--- src/components/AuthLogin/BreakLine.tsx
+++ src/components/AuthLogin/BreakLine.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BreakLine = ({ label }: { label: string }) => {
   return (
-    <div className="mt-2 mb-4 flex whitespace-nowrap w-full justify-center items-center">
+    <div className="flex whitespace-nowrap w-full justify-center items-center">
       <hr className="w-full border-tertiary" />
       <span className="m-2 w-min rounded-lg text-white text-sm uppercase">
         {label}
