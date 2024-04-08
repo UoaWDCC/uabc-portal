b0b17e5bb7db94db28fdf0ff5a6da48276062f82
Tulza <40633228+tulza@users.noreply.github.com>
2024-04-08 17:31:16 +1200

    storybook import fix again oops

--- src/components/AuthLogin/GoogleSignIn.stories.tsx
+++ src/components/AuthLogin/GoogleSignIn.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import GoogleSignIn from "./GoogleSignIn";
+import { GoogleSignIn } from "./GoogleSignIn";
 
 const meta = {
   title: "GoogleSignInButton",
