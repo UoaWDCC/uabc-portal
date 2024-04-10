35cab7891b72289b47ee4d2ab02993bff358f646
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2024-04-11 02:21:08 +1200

    Update SelectableCard styling to change opacity on hover

--- src/components/booking/SelectSessionList/SelectableCard.tsx
+++ src/components/booking/SelectSessionList/SelectableCard.tsx
@@ -17,7 +17,9 @@ export const SelectableCard = ({
   <div
     data-testid="session-card"
     key={session.id}
-    className={session.isFull ? "pointer-events-none" : "cursor-pointer"}
+    className={
+      session.isFull ? "pointer-events-none" : "cursor-pointer hover:opacity-90"
+    }
     onClick={() => !session.isFull && handleSessionClick(session.id)}
     role="checkbox"
     aria-checked={checked}
