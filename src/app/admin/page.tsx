7ccfe041dc104082904fb5208a76ff65e31a465f
HamishPatel <hpat874@aucklanduni.ac.nz>
2024-04-22 00:13:03 +1200

    added two buttons

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,12 +5,12 @@ export default function SelectAdminPage() {
   return (
     <div className="flex h-dvh flex-col">
       <div className="flex p-4">
-        <Heading>Dashboard</Heading>
+        <Heading>Admin Dashboard</Heading>
       </div>
 
       <div className="mt-6 mb-8 mx-4 flex justify-center">
         <Button large className="w-full">
-          Edit Session Schedule
+          Edit Schedule
         </Button>
       </div>
       <div className="mt-6 mb-8 mx-4 flex justify-center">
