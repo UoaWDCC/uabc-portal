d12005fd5a2a28394339d5dd4edf57971c9561c7
Tulza <40633228+tulza@users.noreply.github.com>
2024-05-01 02:39:04 +1200

    fixed alignment

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -29,13 +29,12 @@ export const TextInput = ({
           isError && "!border-destructive !text-destructive",
         )}
         {...props}
-        required
       />
       <span
         className={cn(
           "absolute top-0 -translate-y-[50%] left-2 transition-all pointer-events-none select-none z-10 bg-background px-1 text-primary text-xs autofill:bg-black",
-          "peer-focus:top-0 peer-focus:-translate-y-[50%] peer-focus:text-primary peer-focus:text-xs",
-          "peer-placeholder-shown:top-[50%] peer-placeholder-shown:-translate-y-[50%] peer-placeholder-shown:text-tertiary/70 peer-placeholder-shown:text-base",
+          "peer-focus:top-0 peer-focus:-translate-y-[50%] peer-focus:text-primary peer-focus:text-xs peer-focus:px-1",
+          "peer-placeholder-shown:top-[50%] peer-placeholder-shown:-translate-y-[50%] peer-placeholder-shown:text-tertiary/70 peer-placeholder-shown:text-base peer-placeholder-shown:px-0",
           isError && "!text-destructive",
         )}
       >
