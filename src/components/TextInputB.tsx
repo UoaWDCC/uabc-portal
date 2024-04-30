151fcba379b4f73ae11e8222e2bf461339613858
Tulza <40633228+tulza@users.noreply.github.com>
2024-04-30 23:54:27 +1200

    add type req param, and styling changes

--- src/components/TextInputB.tsx
+++ src/components/TextInputB.tsx
@@ -7,28 +7,35 @@ import { cn } from "@/lib/utils";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  type: string;
   isError: boolean;
-  className: string;
+  classNames?: string;
 }
 
-const TextInputB = ({ label, isError, className, ...props }: InputProps) => {
+const TextInputB = ({
+  label,
+  type,
+  isError,
+  className,
+  ...props
+}: InputProps) => {
   return (
     <div className={cn("relative h-11 ", className)}>
       <input
+        type={type}
         placeholder=" "
         className={twMerge(
           "peer w-full border-primary rounded p-2 border focus:border-2 outline-none h-full bg-background text-tertiary dark:text-white/70",
           isError && "border-red-500 ",
         )}
         {...props}
+        required
       />
       <h2
-        className={twMerge(
-          cn(
-            "absolute -top-3 left-2 transition-all pointer-events-none z-10 bg-background px-2 text-primary text-xs",
-            "peer-focus:-top-3 peer-focus:text-primary peer-focus:translate-y-[0%] peer-focus:text-xs",
-            "peer-placeholder-shown:top-[50%] peer-placeholder-shown:-translate-y-[50%] peer-placeholder-shown:left-1 peer-placeholder-shown:text-white/70 peer-placeholder-shown:text-sm",
-          ),
+        className={cn(
+          "absolute -top-3 left-2 transition-all pointer-events-none select-none z-10 bg-background px-2 text-primary text-xs",
+          "peer-focus:-top-3 peer-focus:translate-y-[0%] peer-focus:text-primary peer-focus:text-xs",
+          "peer-placeholder-shown:top-[50%] peer-placeholder-shown:-translate-y-[50%] peer-placeholder-shown:text-white/70 peer-placeholder-shown:text-sm",
           isError && "text-red-500",
         )}
       >
