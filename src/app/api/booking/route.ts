c396fa6c0d5b9fb202a94a9bf68305b043185e87
Eric Zheng <eric.zheng@outlook.com>
2024-05-01 20:07:10 +1200

    refactor: error messages

--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -17,12 +17,16 @@ interface Session {
 export async function POST(request: Request) {
   try {
     const currentUser = await getCurrentUser();
-    if (currentUser === null) return new Response(null, { status: 401 });
+    if (currentUser === null)
+      return new Response("unauthorized user", { status: 401 });
 
     const json = await request.json();
     const num_of_sessions = json.length;
 
-    if (num_of_sessions === 0) return new Response(null, { status: 400 });
+    if (num_of_sessions === 0)
+      return new Response("num_of_sessions cannot be equal to 0", {
+        status: 400,
+      });
 
     const user = await db.query.users.findFirst({
       where: eq(users.id, currentUser!.id),
@@ -30,7 +34,9 @@ export async function POST(request: Request) {
 
     if (user?.member === true) {
       if (num_of_sessions > 2) {
-        return new Response(null, { status: 400 });
+        return new Response("num_of_sessions cannot be greater than 2", {
+          status: 400,
+        });
       } else {
         for (const session of json) {
           const gameSession = await db.query.gameSessions.findFirst({
@@ -53,29 +59,27 @@ export async function POST(request: Request) {
               remainingSessions: user.remainingSessions - num_of_sessions,
             })
             .where(eq(users.id, currentUser!.id));
-          await tx
-            .insert(bookings)
-            .values({
-              userId: currentUser!.id,
-              gameSessionId: json[0].gamesessionId,
-              createdAt: new Date(),
-              difficulty: json[0].playLevel,
-            });
-          await tx
-            .insert(bookings)
-            .values({
-              userId: currentUser!.id,
-              gameSessionId: json[1].gamesessionId,
-              createdAt: new Date(),
-              difficulty: json[1].playLevel,
-            });
+          await tx.insert(bookings).values({
+            userId: currentUser!.id,
+            gameSessionId: json[0].gamesessionId,
+            createdAt: new Date(),
+            difficulty: json[0].playLevel,
+          });
+          await tx.insert(bookings).values({
+            userId: currentUser!.id,
+            gameSessionId: json[1].gamesessionId,
+            createdAt: new Date(),
+            difficulty: json[1].playLevel,
+          });
         });
 
-        return new Response(null, { status: 204 });
+        return new Response("success", { status: 204 });
       }
     } else {
       if (num_of_sessions !== 1) {
-        return new Response(null, { status: 400 });
+        return new Response("num_of_sessions is not equal to 1", {
+          status: 400,
+        });
       } else {
         const gameSession = await db.query.gameSessions.findFirst({
           where: eq(gameSessions.id, json[0].gamesessionId),
@@ -96,24 +100,18 @@ export async function POST(request: Request) {
               remainingSessions: user!.remainingSessions - num_of_sessions,
             })
             .where(eq(users.id, currentUser!.id));
-          await tx
-            .insert(bookings)
-            .values({
-              userId: currentUser!.id,
-              gameSessionId: json[0].gamesessionId,
-              createdAt: new Date(),
-              difficulty: json[0].playLevel,
-            });
+          await tx.insert(bookings).values({
+            userId: currentUser!.id,
+            gameSessionId: json[0].gamesessionId,
+            createdAt: new Date(),
+            difficulty: json[0].playLevel,
+          });
         });
 
-        return new Response(null, { status: 204 });
+        return new Response("success", { status: 204 });
       }
     }
-
-    // await db.update(gameSessions).set(data).where({ id });
-
-    return new Response(null, { status: 204 });
   } catch {
-    return new Response(null, { status: 500 });
+    return new Response("unexpected error", { status: 500 });
   }
 }
