9e684f22f4c106d011e3d0af9ac18f44851bb7c0
dyzhuu <davidyzhu1973@gmail.com>
2024-05-02 19:04:29 +1200

    clip

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -20,12 +20,12 @@ export const TextInput = ({
   ...props
 }: InputProps) => {
   return (
-    <div className={cn("relative h-11 ", className)}>
+    <div className={cn("relative h-11 overflow-x-clip", className)}>
       <input
         type={type}
         placeholder={props.placeholder ? props.placeholder : " "}
         className={twMerge(
-          "peer w-full border-primary rounded p-2 border outline-none ring-primary focus:ring-1 h-full bg-background dark:text-white/70",
+          "peer w-full border-primary rounded p-2 border outline-none ring-primary ring-inset focus:ring-1 h-full bg-background dark:text-white/70",
           isError && "border-destructive ring-destructive",
         )}
         {...props}
@@ -33,8 +33,8 @@ export const TextInput = ({
       <span
         className={cn(
           "absolute top-0 -translate-y-[50%] left-2 transition-all pointer-events-none select-none z-10 bg-background px-1 text-primary text-xs whitespace-nowrap",
-          "peer-focus:top-0 peer-focus:text-primary peer-focus:text-xs peer-focus:px-1",
-          "peer-placeholder-shown:top-[50%] peer-placeholder-shown:text-tertiary/70 peer-placeholder-shown:text-base peer-placeholder-shown:px-0",
+          "peer-focus:top-0 peer-focus:text-primary peer-focus:text-xs peer-focus:px-1 peer-focus:bg-background",
+          "peer-placeholder-shown:top-[50%] peer-placeholder-shown:text-tertiary/70 peer-placeholder-shown:text-base peer-placeholder-shown:px-0 peer-placeholder-shown:bg-transparent",
           isError && "text-destructive",
         )}
       >
