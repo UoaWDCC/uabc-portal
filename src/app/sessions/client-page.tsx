0581f983e1bc4095ca8b51d98a34957fe9d4c0f4
Veni <drcubing27@gmail.com>
2024-05-07 09:57:50 +1200

    Move static html

--- src/app/sessions/client-page.tsx
+++ src/app/sessions/client-page.tsx
@@ -11,27 +11,18 @@ import { useRouter } from "next/navigation";
 import { CgProfile } from "react-icons/cg";
 import { twJoin } from "tailwind-merge";
 
-import { PrepaidSessionsCounter } from "@/components/booking/PrepaidSessionsCounter";
 import { SelectSessionList } from "@/components/booking/SelectSessionList/SelectSessionList";
 import { CountIndicator } from "@/components/CountIndicator";
-import { Heading } from "@/components/Heading";
 import { Button } from "@/components/ui/button";
 import { MEMBER_MAX_SESSIONS, NON_MEMBER_MAX_SESSIONS } from "@/lib/constants";
 import { useCartStore } from "@/stores/useCartStore";
 
-//const remainingSessions = 11;
-//const isMember = true;
-//const firstName = "David";
 interface ClientSessionPageProps {
-  firstName: string;
   isMember: boolean;
-  remainingSessions: number;
 }
 
 export default function ClientSessionPage({
-  firstName,
   isMember,
-  remainingSessions,
 }: ClientSessionPageProps) {
   const { push } = useRouter();
 
@@ -41,27 +32,6 @@ export default function ClientSessionPage({
 
   return (
     <div className="flex h-dvh flex-col">
-      <div className="flex p-4">
-        <Heading>Sessions</Heading>
-        <Link href={"/account"} className="ml-auto">
-          <CgProfile size={40} />
-        </Link>
-      </div>
-      <div className="flex h-16 items-center justify-between bg-secondary/70 p-4">
-        <div className="flex items-center">
-          <span className="pr-1 font-medium">Hey {firstName}!</span>
-          <Image
-            src="/images/BadmintonRacketLogo.png"
-            alt="Badminton Racket Logo"
-            className="pointer-events-none select-none"
-            width={20}
-            height={20}
-          />
-        </div>
-        {isMember && (
-          <PrepaidSessionsCounter remainingSessions={remainingSessions} />
-        )}
-      </div>
       <div className="flex h-16 items-center justify-between p-4">
         <p className="max-w-[70%] font-medium text-sm">
           Please select a badminton session for this week
--- src/app/sessions/page.tsx
+++ src/app/sessions/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { CgProfile } from "react-icons/cg";
 
+import { PrepaidSessionsCounter } from "@/components/booking/PrepaidSessionsCounter";
+import { Heading } from "@/components/Heading";
 import { env } from "@/env";
 import { getCurrentUser } from "@/lib/session";
 import ClientSessionPage from "./client-page";
@@ -20,14 +25,36 @@ const fetchUserInfo = async (id: string): Promise<UserResponse> => {
   });
   return response.json();
 };
+
 export default async function SelectSessionPage() {
   const user = await getCurrentUser();
   const userdata = await fetchUserInfo(user!.id);
   return (
-    <ClientSessionPage
-      firstName={userdata.firstName}
-      isMember={userdata.member}
-      remainingSessions={userdata.remainingSessions}
-    ></ClientSessionPage>
+    <div>
+      <div className="flex p-4">
+        <Heading>Sessions</Heading>
+        <Link href={"/account"} className="ml-auto">
+          <CgProfile size={40} />
+        </Link>
+      </div>
+      <div className="flex h-16 items-center justify-between bg-secondary/70 p-4">
+        <div className="flex items-center">
+          <span className="pr-1 font-medium">Hey {userdata.firstName}!</span>
+          <Image
+            src="/images/BadmintonRacketLogo.png"
+            alt="Badminton Racket Logo"
+            className="pointer-events-none select-none"
+            width={20}
+            height={20}
+          />
+        </div>
+        {userdata.member && (
+          <PrepaidSessionsCounter
+            remainingSessions={userdata.remainingSessions}
+          />
+        )}
+      </div>
+      <ClientSessionPage isMember={userdata.member} />
+    </div>
   );
 }
