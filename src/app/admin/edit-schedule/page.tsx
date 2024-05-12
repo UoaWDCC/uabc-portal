397951f7f8cb6b9cd589525d03b11389987694c6
Tulza <40633228+tulza@users.noreply.github.com>
2024-05-12 20:29:54 +1200

    init dialogue

--- src/app/admin/edit-schedule/page.tsx
+++ src/app/admin/edit-schedule/page.tsx
@@ -2,6 +2,7 @@ import { env } from "process";
 import Link from "next/link";
 import { ArrowLeft, Ellipsis, Plus, SquarePen, Trash2 } from "lucide-react";
 
+import EditDialogue from "@/admin/EditDialogue";
 import { SessionCard } from "@/admin/SessionCard";
 import { Card } from "@/components/Card";
 import { Button } from "@/components/ui/button";
@@ -30,31 +31,34 @@ const page = async () => {
 
   const user = await fetchUserInfo(currentUser!.id);
   return (
-    <div className="flex px-4 h-dvh w-dvw flex-col bg-background dark">
-      <div className="py-4 flex justify-between">
-        <div className="flex items-center">
-          <Link
-            href="/admin"
-            className="h-full aspect-square grid place-items-center rounded-full hover:bg-tertiary/10 bg-[#0000] transition-color"
-          >
-            <ArrowLeft className="stroke-tertiary" />
-          </Link>
-          <div className="ml-3 text-tertiary text-lg font-medium">
-            Edit schedules
+    <>
+      <EditDialogue />
+      <div className="flex px-4 h-dvh w-dvw flex-col bg-background dark">
+        <div className="py-4 flex justify-between">
+          <div className="flex items-center">
+            <Link
+              href="/admin"
+              className="h-full aspect-square grid place-items-center rounded-full hover:bg-tertiary/10 bg-[#0000] transition-color"
+            >
+              <ArrowLeft className="stroke-tertiary" />
+            </Link>
+            <div className="ml-3 text-tertiary text-lg font-medium">
+              Edit schedules
+            </div>
           </div>
+          <Button
+            variant="outline"
+            className="border-2 aspect-square border-secondary"
+          >
+            <Plus className="absolute stroke-foreground w-5" />
+          </Button>
         </div>
-        <Button
-          variant="outline"
-          className="border-2 aspect-square border-secondary"
-        >
-          <Plus className="absolute stroke-foreground w-5" />
-        </Button>
+        <div className="gap-4 flex flex-col">
+          <SessionCard />
+          <SessionCard />
+        </div>
       </div>
-      <div className="gap-4 flex flex-col">
-        <SessionCard />
-        <SessionCard />
-      </div>
-    </div>
+    </>
   );
 };
 
