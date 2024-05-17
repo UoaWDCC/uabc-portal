8e7a0c8d9239eac62525c74ae94d874665d1c2dc
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2024-05-17 20:40:13 +1200

    Fix incorrect param name

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -10,13 +10,13 @@ import { users } from "@/lib/db/schema";
  */
 export async function GET(
   _req: NextRequest,
-  { params }: { params: { id: string } },
+  { params }: { params: { userId: string } },
 ) {
   try {
-    const { id } = params;
+    const { userId } = params;
 
     const user = await db.query.users.findFirst({
-      where: eq(users.id, id),
+      where: eq(users.id, userId),
       columns: {
         id: true,
         firstName: true,
@@ -29,7 +29,7 @@ export async function GET(
     });
 
     if (!user) {
-      return new Response(`No User found for id: ${id}`, { status: 404 });
+      return new Response(`No User found for id: ${userId}`, { status: 404 });
     }
 
     return NextResponse.json(user);
