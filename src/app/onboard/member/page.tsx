dab077c87cfb26d4ea0c6c87677108240f723fa2
Kimia Varasteh <kimiavarasteh@Kimias-Laptop.fritz.box>
2024-05-20 12:32:51 +1200

    added the api call in members page

--- src/app/onboard/member/page.tsx
+++ src/app/onboard/member/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 import { MembershipTypeSelector } from "@/components/MembershipTypeSelector";
 import { NavigationBar } from "@/components/NavigationBar";
@@ -10,9 +11,26 @@ import { useOnboardingDetailsStore } from "@/stores/useOnboardingDetailsStore";
 const MembershipType = () => {
   const member = useOnboardingDetailsStore((state) => state.member);
   const setMember = useOnboardingDetailsStore((state) => state.setMember);
+  const firstName = useOnboardingDetailsStore((state) => state.firstName);
+  const lastName = useOnboardingDetailsStore((state) => state.lastName);
   const router = useRouter();
 
-  const handleNextButtonClick = () => {
+  const session = useSession();
+  const id = session.data!.user!.id;
+
+  const handleNextButtonClick = async () => {
+    const user = session.data!.user!;
+    user.member = member;
+    user.firstName = firstName;
+    user.lastName = lastName;
+    const response = await fetch(`/api/users/${id}/onboard`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ firstName, lastName, member }),
+    });
+    console.log(response);
     router.push("/sessions");
   };
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,32 @@ import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
+  console.log(token);
+
   const isAuth = !!token;
   const fromUrl = req.nextUrl.pathname;
   const isAuthPage =
     fromUrl.startsWith("/auth/login") || fromUrl.startsWith("/auth/signup");
 
   if (isAuthPage) {
-    if (isAuth) {
+    if (
+      (isAuth! &&
+        (token as { profile: { firstName: string } }).profile.firstName) ||
+      !(token as { profile: { lastName: string } }).profile.lastName
+    ) {
       return NextResponse.redirect(new URL("/sessions", req.url));
+    }
+
+    if (
+      (token as { profile: { firstName: string } }).profile.firstName &&
+      (token as { profile: { lastName: string } }).profile.lastName &&
+      (token as { profile: { member: boolean } }).profile.member !== null
+    ) {
+      return NextResponse.redirect(new URL("/sessions", req.url));
+    } else if (
+      (token as { profile: { member: boolean } }).profile.member === null
+    ) {
+      return NextResponse.redirect(new URL("/onboard/membership", req.url));
     }
 
     return null;
@@ -20,6 +38,23 @@ export async function middleware(req: NextRequest) {
   if (!isAuth) {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
+  //if last name or first name null redirect to name page
+  //   if (
+  //     !(token as { profile: { firstName: string } }).profile.firstName ||
+  //     !(token as { profile: { lastName: string } }).profile.lastName
+  //   ) {
+  //     return NextResponse.redirect(new URL("/onboard/name", req.url));
+  //   }
+
+  // if (
+  //   (token as { profile: { firstName: string } }).profile.firstName &&
+  //   (token as { profile: { lastName: string } }).profile.lastName &&
+  //   (token as { profile: { member: boolean } }).profile.member !== null
+  // ) {
+  //   return NextResponse.redirect(new URL("/sessions", req.url));
+  // } else if ((token as { profile: { member: boolean } }).profile.member === null) {
+  //   return NextResponse.redirect(new URL("/onboard/membership", req.url));
+  // }
 
   return NextResponse.next();
 }
