500845d1cf6b6c81055f61c35fbfed7535e4914e
Kimia Varasteh <kimiavarasteh@Kimias-Laptop.fritz.box>
2024-05-31 20:13:39 +1200

    Add download route for game sessions
    new route added

--- src/app/api/game-sessions/[gameSessionId]/download/route.ts
+++ src/app/api/game-sessions/[gameSessionId]/download/route.ts
@@ -1,60 +1,3 @@
-// import type { NextRequest } from "next/server";
-// import { NextResponse } from "next/server";
-// import { eq } from "drizzle-orm";
-// import { z } from "zod";
-
-// import { db } from "@/lib/db";
-// import { bookingDetails, bookings, gameSessions, users } from "@/lib/db/schema";
-
-// const routeContextSchema = z.object({
-//   params: z.object({
-//     gameSessionId: z.coerce.number(),
-//   }),
-// });
-
-// export async function GET(
-//   _req: NextRequest,
-//   context: z.infer<typeof routeContextSchema>,
-// ) {
-//   try {
-//     const result = routeContextSchema.safeParse(context);
-//     if (!result.success)
-//       return new Response("Invalid id provided in the request", {
-//         status: 400,
-//       });
-
-//     const {
-//       params: { gameSessionId },
-//     } = result.data;
-
-//     const session = await db.query.gameSessions.findFirst({
-//       where: eq(gameSessions.id, gameSessionId),
-//     });
-
-//     if (!session)
-//       return new Response(`No Game Session found with id: ${gameSessionId}`, {
-//         status: 404,
-//       });
-//         const [players] = await db
-//         .select({
-//           firstName: users.firstName,
-//           lastName: users.lastName,
-//           email: users.email,
-//           playLevel: bookingDetails.playLevel,
-//           member: users.member,
-
-//         })
-//         .from(gameSessions)
-//         .innerJoin(bookingDetails, eq(gameSessions.id, bookingDetails.gameSessionId))
-//         .innerJoin(bookings, eq(bookingDetails.bookingId, bookings.id))
-//         .rightJoin(users, eq(bookings.userId, users.id))
-//         .where(eq(gameSessions.id, gameSessionId)).execute();
-
-//     return NextResponse.json(players);
-//   } catch {
-//     return new Response("Internal Server Error", { status: 500 });
-//   }
-// }
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { eq } from "drizzle-orm";
@@ -75,12 +18,10 @@ export async function GET(
 ) {
   try {
     const result = routeContextSchema.safeParse(context);
-    if (!result.success) {
+    if (!result.success)
       return new Response("Invalid id provided in the request", {
         status: 400,
       });
-    }
-    console.log("helloooooooooo");
 
     const {
       params: { gameSessionId },
@@ -90,13 +31,11 @@ export async function GET(
       where: eq(gameSessions.id, gameSessionId),
     });
 
-    if (!session) {
+    if (!session)
       return new Response(`No Game Session found with id: ${gameSessionId}`, {
         status: 404,
       });
-    }
-
-    const players = await db
+    const [players] = await db
       .select({
         firstName: users.firstName,
         lastName: users.lastName,
@@ -110,13 +49,12 @@ export async function GET(
         eq(gameSessions.id, bookingDetails.gameSessionId),
       )
       .innerJoin(bookings, eq(bookingDetails.bookingId, bookings.id))
-      .innerJoin(users, eq(bookings.userId, users.id))
+      .rightJoin(users, eq(bookings.userId, users.id))
       .where(eq(gameSessions.id, gameSessionId))
       .execute();
 
     return NextResponse.json(players);
-  } catch (error) {
-    console.error(error);
+  } catch {
     return new Response("Internal Server Error", { status: 500 });
   }
 }
