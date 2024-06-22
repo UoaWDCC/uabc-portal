312adda61210a2af95e7cce6a239a076902e81ee
Veni <drcubing27@gmail.com>
2024-06-22 21:06:44 +1200

    Fix edge case where day is outside semester dates, and also add ability to add sessions from next booking open date

--- src/app/api/cronjob/route.ts
+++ src/app/api/cronjob/route.ts
@@ -1,12 +1,6 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import {
-  addDays,
-  eachDayOfInterval,
-  format,
-  isWithinInterval,
-  subDays,
-} from "date-fns";
+import { addDays, eachDayOfInterval, format, isWithinInterval } from "date-fns";
 import { and, eq, gte, lte } from "drizzle-orm";
 
 import { db } from "@/lib/db";
@@ -20,9 +14,11 @@ import { insertGameSessionSchema } from "@/lib/validators";
 
 export async function POST(req: NextRequest) {
   try {
-    const now = new Date();
-    const oneWeekFromNow = addDays(now, 7);
+    // get current day and reset to 12am
+    let now: Date = new Date();
+    now = new Date(now.getTime() - (now.getTime() % (3600 * 24 * 1000)));
 
+    // Find current semester
     const activeSemester = await db.query.semesters.findFirst({
       where: and(
         lte(semesters.startDate, format(now, "yyyy-MM-dd")),
@@ -45,14 +41,55 @@ export async function POST(req: NextRequest) {
       });
     }
 
-    const daysOfWeek = eachDayOfInterval({ start: now, end: oneWeekFromNow });
+    // Find the day of the week that booking opens
+    const dayNow = now.getDay();
+    const bookingOpenDay = activeSemester.bookingOpenDay;
+    let dayNum: number;
+    switch (bookingOpenDay) {
+      case "Monday":
+        dayNum = 1;
+        break;
+      case "Tuesday":
+        dayNum = 2;
+        break;
+      case "Wednesday":
+        dayNum = 3;
+        break;
+      case "Thursday":
+        dayNum = 4;
+        break;
+      case "Friday":
+        dayNum = 5;
+        break;
+      case "Saturday":
+        dayNum = 6;
+        break;
+      default:
+        dayNum = 0;
+    }
+
+    let diff: number = dayNum + 7 - dayNow;
+
+    if (diff > 7) {
+      diff -= 7;
+    }
+
+    // Find the day of the week that booking opens + 7 days (next batch of sessions)
+    const startDate = addDays(now, diff);
+    const oneWeekFromStart = addDays(startDate, 7);
+    const daysOfWeek = eachDayOfInterval({ start: now, end: oneWeekFromStart });
 
     await db.transaction(async (tx) => {
+      // For each day in the session week, create a game session
       for (const day of daysOfWeek) {
+        if (day > new Date(activeSemester.endDate)) {
+          break;
+        }
         const dayOfWeek = day.getDay();
 
         //check for gameSessionScheduleExceptions and continue if deleted, and if edited then insert that instead and continue
 
+        // Find game session schedule for the day
         const schedule = await tx.query.gameSessionSchedules.findFirst({
           where: and(
             eq(
@@ -67,9 +104,15 @@ export async function POST(req: NextRequest) {
           continue;
         }
 
+        // Calculate booking close time
+        const [hours, mins, secs] = schedule.startTime.split(":").map(Number);
+        const bookingClose = new Date(
+          day.getTime() + 3600 * 1000 * hours + 60 * 1000 * mins + 1000 * secs,
+        );
+
         const insertSchedule = insertGameSessionSchema.parse({
-          bookingOpen: subDays(day, 2), //placeholder
-          bookingClose: subDays(day, 1), //placeholder
+          bookingOpen: startDate,
+          bookingClose: bookingClose,
           gameSessionScheduleId: schedule.id,
           date: day,
           startTime: schedule.startTime,
