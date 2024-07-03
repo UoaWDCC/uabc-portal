f153c121b3f41d68e766583b2420503323e71dd8
dyzhuu <davidyzhu1973@gmail.com>
2024-07-04 02:21:08 +1200

    feat: add approve and reject api routes

---
+++ src/app/api/users/[userId]/membership/reject/route.ts
@@ -0,0 +1,39 @@
+import type { NextRequest } from "next/server";
+import { eq } from "drizzle-orm";
+
+import { db } from "@/lib/db";
+import { users } from "@/lib/db/schema";
+import { getCurrentUser } from "@/lib/session";
+
+export async function PATCH(
+  _req: NextRequest,
+  { params }: { params: { userId: string } },
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return new Response("ERROR: Unauthorized request", { status: 401 });
+  }
+
+  if (currentUser.role !== "admin") {
+    return new Response("ERROR: No valid permissions", { status: 403 });
+  }
+
+  const { userId } = params;
+
+  const user = await db
+    .update(users)
+    .set({ member: false })
+    .where(eq(users.id, userId))
+    .returning();
+
+  if (!user) {
+    return new Response(`No user found with id: ${userId}`, {
+      status: 404,
+    });
+  }
+
+  // TODO: Send email to user
+
+  return new Response(null, { status: 204 });
+}
