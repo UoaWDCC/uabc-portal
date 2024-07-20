1e9ea3c058cc47bd2813e3c42053b9b741006b78
dyzhuu <davidyzhu1973@gmail.com>
2024-07-20 17:46:21 +1200

    chore: add logging to cron

--- cron/index.mjs
+++ cron/index.mjs
@@ -1,15 +1,20 @@
 export const handler = async () => {
-  if (!process.env.APP_URL)
+  console.log("Function invoked");
+  if (!process.env.APP_URL) {
+    console.error("APP_URL is not set");
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "APP_URL is not set" }),
     };
+  }
 
-  if (!process.env.API_KEY)
+  if (!process.env.API_KEY) {
+    console.error("API_KEY is not set");
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "API_KEY is not set" }),
     };
+  }
 
   const endpointUrl = `${process.env.APP_URL}/api/cron/generate-sessions`;
 
@@ -23,11 +28,14 @@ export const handler = async () => {
 
     const body = await res.text();
 
+    console.log(body);
+
     return {
       statusCode: res.status,
       body,
     };
   } catch {
+    console.error("Failed to invoke function");
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Failed to call endpoint" }),
