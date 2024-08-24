bac25430ba8d10dceb13ec5e9e2a34d9a25e14e5
annabelle2023 <adin525@aucklanduni.ac.nz>
2024-08-24 14:40:47 +1200

    make login page responsive

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -13,11 +13,14 @@ export default function AuthLayout({
     <div className="grid min-h-dvh w-dvw place-items-center sm:p-8">
       <Card
         variant="card"
-        className="flex h-full max-h-[1000px] w-full flex-col justify-between border-0 bg-transparent py-8 sm:max-w-xl sm:border sm:bg-card"
+        className="flex h-full max-h-[1000px] w-full flex-col justify-between border-0 bg-transparent py-8 sm:max-w-xl sm:border sm:bg-card md:max-w-3xl md:gap-8 md:px-16 md:py-16 lg:max-w-5xl lg:flex-row lg:gap-8 lg:px-24 lg:py-48"
       >
-        <UabcHeaderText className="mb-4" />
-        <UabcLogo className="flex justify-center" />
-        <div className="flex w-full flex-col gap-4">{children}</div>
+        <div className="lg:w-1/3">
+          <UabcHeaderText className="mb-4" />
+          <UabcLogo className="flex justify-center" />
+        </div>
+
+        <div className="flex w-full flex-col gap-4 lg:w-2/3">{children}</div>
       </Card>
     </div>
   );
