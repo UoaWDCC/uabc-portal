8ed680212951240f4ec809665dd8651bb0707208
annabelle2023 <adin525@aucklanduni.ac.nz>
2024-08-26 00:11:08 +1200

    minor changes

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -13,14 +13,16 @@ export default function AuthLayout({
     <div className="grid min-h-dvh w-dvw place-items-center sm:p-8">
       <Card
         variant="card"
-        className="flex h-full max-h-[1000px] w-full flex-col justify-between border-0 bg-transparent py-8 sm:max-w-xl sm:border sm:bg-card md:max-w-3xl md:gap-8 md:px-16 md:py-16 lg:max-w-5xl lg:flex-row lg:gap-8 lg:px-24 lg:py-48"
+        className="sm:max-h[780px] md:max-h[780px] lg:max-h[600px] flex h-full w-full flex-col justify-center border-0 bg-transparent py-8 sm:max-w-[440px] sm:border sm:bg-card md:max-w-[440px] md:gap-8 md:px-16 md:py-16 lg:max-w-[900px] lg:flex-row lg:gap-8 lg:px-24 lg:py-24"
       >
-        <div className="lg:w-1/3">
+        <div className="lg:flex lg:w-1/2 lg:flex-col lg:justify-center">
           <UabcHeaderText className="mb-4" />
           <UabcLogo className="flex justify-center" />
         </div>
 
-        <div className="flex w-full flex-col gap-4 lg:w-2/3">{children}</div>
+        <div className="flex flex-col gap-4 lg:w-1/2 lg:justify-center">
+          {children}
+        </div>
       </Card>
     </div>
   );
