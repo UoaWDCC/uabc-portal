6562963a25b03c57cabb51f6ca27e500cfb25e75
dyzhuu <davidyzhu1973@gmail.com>
2024-09-07 16:38:34 +1200

    fix: types * added types for wrappers * installed @types/pg

--- src/lib/wrappers.ts
+++ src/lib/wrappers.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 
 import { getCurrentUser } from "@/lib/session";
 import type { User } from "@/types/next-auth";
+import { StatusError } from "./exceptions";
 
 type EndpointOptions = {
   protected?: boolean;
@@ -17,6 +18,13 @@ type Handler = (
   user?: User | undefined
 ) => unknown;
 
+type UserRouteHandler = (
+  req: NextRequest,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ctx: any,
+  user: User
+) => unknown;
+
 const defaultEndpointOptions = { protected: false, admin: false };
 
 export function routeWrapper(
@@ -25,8 +33,8 @@ export function routeWrapper(
 ) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return async (req: NextRequest, ctx: any) => {
-    // check if request is authenticated
     try {
+      // check if request is authenticated
       const user = await getCurrentUser();
 
       if ((options.protected || options.admin) && !user)
@@ -36,8 +44,14 @@ export function routeWrapper(
         return new Response("ERROR: No valid permissions", { status: 403 });
 
       // return the route handler
-      return handler(req, ctx, user);
+      return await handler(req, ctx, user);
     } catch (error) {
+      if (error instanceof StatusError) {
+        return NextResponse.json(
+          { message: error.message },
+          { status: error.status }
+        );
+      }
       if (error instanceof z.ZodError) {
         return NextResponse.json({ errors: error.issues }, { status: 400 });
       }
@@ -51,10 +65,14 @@ export function routeWrapper(
   };
 }
 
-export function userRouteWrapper(handler: Handler) {
-  return routeWrapper(handler, { admin: true });
+export function userRouteWrapper(handler: UserRouteHandler) {
+  return routeWrapper((req, ctx, user) => handler(req, ctx, user!), {
+    admin: true,
+  });
 }
 
-export function adminRouteWrapper(handler: Handler) {
-  return routeWrapper(handler, { protected: true });
+export function adminRouteWrapper(handler: UserRouteHandler) {
+  return routeWrapper((req, ctx, user) => handler(req, ctx, user!), {
+    protected: true,
+  });
 }
