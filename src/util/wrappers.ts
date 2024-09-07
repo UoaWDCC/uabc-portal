ce5b49ae2c65c4eeec77d80abd6d2be75367f45e
dyzhuu <davidyzhu1973@gmail.com>
2024-09-07 15:58:32 +1200

    refactor wrappers

--- src/util/wrappers.ts
+++ src/util/wrappers.ts
@@ -1,49 +1,60 @@
-import { getCurrentUser } from '@/lib/session';
-import { NextRequest, NextResponse } from 'next/server';
-import { User } from '@/types/next-auth';
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { z } from "zod";
 
-export const withError = (
-  handler: (req: NextRequest, res: NextResponse, user: User) => any
-) => {
-  return async (req: NextRequest, res: NextResponse) => {
+import { getCurrentUser } from "@/lib/session";
+import type { User } from "@/types/next-auth";
+
+type EndpointOptions = {
+  protected?: boolean;
+  admin?: boolean;
+};
+
+type Handler = (
+  req: NextRequest,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ctx?: any,
+  user?: User | undefined
+) => unknown;
+
+const defaultEndpointOptions = { protected: false, admin: false };
+
+export function routeWrapper(
+  handler: Handler,
+  options: EndpointOptions = defaultEndpointOptions
+) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return async (req: NextRequest, ctx: any) => {
+    // check if request is authenticated
     try {
-    const user = await getCurrentUser();
-      return await handler(req, res, user);
-    } catch (error) {
-      console.error('Error:', error);
-      return NextResponse.json({ message: 'Internal Server Error' }, {status: 500});
-    }
-  };
-};
+      const user = await getCurrentUser();
 
-
-
-
-export const withUser = (
-  handler: (req: NextRequest, res: NextResponse, user: User) => any
-) => {
-  return async (req: NextRequest, res: NextResponse) => {
-    const user = await getCurrentUser();
-
-    if (!user) {
+      if ((options.protected || options.admin) && !user)
         return new Response("ERROR: Unauthorized request", { status: 401 });
-    }
 
-    await handler(req, res, user);
-  };
-};
-
-
-export const withAdmin = (
-  handler: (req: NextRequest, res: NextResponse, user: User) => any
-) => {
-  return async (req: NextRequest, res: NextResponse) => {
-    const user = await getCurrentUser();
-
-    if (!user || !user.isAdmin) {
+      if (options.admin && user?.role !== "admin")
         return new Response("ERROR: No valid permissions", { status: 403 });
-    }
 
-    await handler(req, res, user);
+      // return the route handler
+      return handler(req, ctx, user);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return NextResponse.json({ errors: error.issues }, { status: 400 });
+      }
+      // catch all errors
+      console.error("Error:", error);
+      return NextResponse.json(
+        { message: "Internal Server Error" },
+        { status: 500 }
+      );
+    }
   };
-};
+}
+
+export function userRouteWrapper(handler: Handler) {
+  return routeWrapper(handler, { admin: true });
+}
+
+export function adminRouteWrapper(handler: Handler) {
+  return routeWrapper(handler, { protected: true });
+}
