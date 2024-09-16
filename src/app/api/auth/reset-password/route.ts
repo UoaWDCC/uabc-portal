844431f1f2dca8d7a7f825c617fcb9283e1417dc
Eric Zheng <eric.zheng@outlook.com>
2024-09-16 14:21:46 +1200

    feat: finished base functionality

--- src/app/api/auth/reset-password/route.ts
+++ src/app/api/auth/reset-password/route.ts
@@ -5,6 +5,7 @@ import { z } from "zod";
 import { db } from "@/lib/db";
 import { forgotPasswordTokens, users } from "@/lib/db/schema";
 import { deobfuscateSqid } from "@/lib/sqid";
+import { hash } from "bcrypt";
 
 const searchParamSchema = z.object({
   resetPasswordToken: z.string(),
@@ -25,6 +26,37 @@ const postRequestSchema = z.object({
     .regex(/[A-Z]/, { message: "Password must contain an uppercase letter" }),
 });
 
+export async function GET(req: NextRequest) {
+  try {
+    const searchParams = searchParamSchema.parse(
+      Object.fromEntries(req.nextUrl.searchParams)
+    );
+    
+    const token = deobfuscateSqid(searchParams.resetPasswordToken);
+
+    const resetPasswordToken = await db.query.forgotPasswordTokens.findFirst({
+      where: and(
+        eq(forgotPasswordTokens.token, token.toString()),
+        gt(forgotPasswordTokens.expires, new Date())
+      ),
+    });
+
+    if (!resetPasswordToken) {
+      return NextResponse.json("ResetPasswordToken is invalid", {
+        status: 403,
+      });
+    } else {
+      return NextResponse.json(resetPasswordToken.identifier, { status: 200 });
+    }
+  } catch (error : any) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json({ errors: error.issues }, { status: 400 });
+    } else {
+      return NextResponse.json({ errors: "Internal Server Error" }, { status: 500 });
+    }
+  }
+}
+
 export async function POST(req: NextRequest) {
   try {
     const searchParams = searchParamSchema.parse(
@@ -43,7 +75,18 @@ export async function POST(req: NextRequest) {
       return NextResponse.json("ResetPasswordToken is invalid", {
         status: 403,
       });
+    }
+    
+    const user = await db.query.users.findFirst({
+      where: eq(users.email, resetPasswordToken!.identifier)
+    });
+
+    if (!users) {
+      return NextResponse.json("User does not exist", {
+        status: 403,
+      });
     } else {
+      //change the password of the user
       const body = await req.json();
       const { newPassword, confirmPassword } = postRequestSchema.parse(body);
 
@@ -52,6 +95,19 @@ export async function POST(req: NextRequest) {
           status: 400,
         });
       }
+
+      const costFactor = 12;
+      const hashedPassword = await hash(newPassword, costFactor); 
+
+      await db.transaction(async (tx) => {
+        await tx.update(users).set({
+          password: hashedPassword,
+        }).where(eq(users.email, resetPasswordToken!.identifier));
+      });
+
+      return NextResponse.json("Password changed successfully", {
+        status: 200,
+      });
     }
   } catch (error) {
     if (error instanceof z.ZodError) {
