532f1ca5831a5e645893228c8c7af95f8be00dd4
Tulza <40633228+tulza@users.noreply.github.com>
2024-09-19 19:34:49 +1200

    Attendees Table no pagination

--- src/app/admin/view-sessions/[gameSessionId]/page.tsx
+++ src/app/admin/view-sessions/[gameSessionId]/page.tsx
@@ -1,7 +1,8 @@
 import { notFound } from "next/navigation";
 import { z } from "zod";
 
-import { AttendeesList } from "@/components/admin/view-sessions/AttendeesList";
+import { AttendeesTable } from "@/components/admin/view-sessions/gameSessionId/AttendeesList";
+import { NavigationBar } from "@/components/NavigationBar";
 import { getAttendeesFromId } from "@/services/game-sessions";
 
 const routeContextSchema = z.object({
@@ -23,8 +24,18 @@ export default async function ViewSessionsPage(
   if (!attendees) notFound();
 
   return (
-    <div className="grid size-full place-items-center">
-      <AttendeesList gameSessionId={gameSessionId} />
+    <div className="mx-4 flex min-h-dvh flex-col">
+      <NavigationBar title="Members" pathName="/admin" />
+      <div className="flex grow flex-col items-center">
+        <div className="flex w-full flex-col gap-y-4 px-6 py-4 lg:mt-12 lg:w-4/5 lg:min-w-fit lg:px-12">
+          <h1 className="text-2xl font-semibold">Attendees</h1>
+          <p className="text-muted-foreground">
+            Here&apos;s the attendee list for the session on{" "}
+            <strong>Tuesday 19th September.</strong>
+          </p>
+          <AttendeesTable gameSessionId={gameSessionId} />
+        </div>
+      </div>
     </div>
   );
 }
