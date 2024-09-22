e141313a784bed88eaa29950027a9bde876ddb3f
Kimia Varasteh <kimiavarasteh@Kimias-Laptop.fritz.box>
2024-09-23 10:32:42 +1200

    added style changes

--- src/components/SelectDropdown.tsx
+++ src/components/SelectDropdown.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import { useState } from "react";
-import { twMerge } from "tailwind-merge"; // Merge Tailwind classes
 
 import {
   Select,
@@ -18,22 +17,15 @@ const SelectDropdown = () => {
   const [selectedDay, setSelectedDay] = useState("");
 
   return (
-    <div className="flex flex-col space-y-4">
-      <div className="relative">
+    <div>
+      <div>
         <Select onValueChange={setSelectedDay}>
-          <SelectTrigger
-            className={twMerge(
-              "w-[280px] border-2 border-tertiary/70 p-2 outline-none transition-colors focus:border-primary focus:ring-primary",
-              selectedDay && "border-primary text-primary" // Blue border and text when selected
-            )}
-          >
+          <SelectTrigger>
             <SelectValue placeholder="Select a day of the week" />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
-              <SelectLabel className="text-tertiary/70">
-                Days of the Week
-              </SelectLabel>
+              <SelectLabel>Days of the Week</SelectLabel>
               <SelectItem value="monday">Monday</SelectItem>
               <SelectItem value="tuesday">Tuesday</SelectItem>
               <SelectItem value="wednesday">Wednesday</SelectItem>
@@ -45,7 +37,7 @@ const SelectDropdown = () => {
           </SelectContent>
         </Select>
         {selectedDay && (
-          <p className="mt-2 text-primary">
+          <p>
             <strong>
               {selectedDay.charAt(0).toUpperCase() + selectedDay.slice(1)}
             </strong>
