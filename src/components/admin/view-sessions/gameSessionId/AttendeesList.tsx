219b0fdd42c148b0513d5173079c55fb8a591bcb
Tulza <40633228+tulza@users.noreply.github.com>
2024-09-29 04:02:58 +1300

    Add Table sorting functionality + ts fix

--- src/components/admin/view-sessions/gameSessionId/AttendeesList.tsx
+++ src/components/admin/view-sessions/gameSessionId/AttendeesList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import "@tanstack/react-table";
 
@@ -21,8 +21,22 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useAttendees } from "@/hooks/query/useAttendees";
+import {
+  useAttendees,
+  type AttendeesListResponse,
+} from "@/hooks/query/useAttendees";
 import SkeletonAttendeeList from "./EmptySkeletonAttendeeList";
+
+const sortByMember = (a: AttendeesListResponse) => (a.member ? -1 : 1);
+
+const sortByPlayLevel = (a: AttendeesListResponse, b: AttendeesListResponse) =>
+  a.playLevel.localeCompare(b.playLevel);
+
+const sortByEmail = (a: AttendeesListResponse, b: AttendeesListResponse) =>
+  a.email.localeCompare(b.email);
+
+const sortByName = (a: { name: string }, b: { name: string }) =>
+  a.name.localeCompare(b.name);
 
 export const AttendeesTable = ({
   gameSessionId,
@@ -40,24 +54,69 @@ export const AttendeesTable = ({
       }),
     [data]
   );
+  const [sortedPlayers, setPlayers] = useState({
+    sortid: "none",
+    attendees: attendees,
+  });
+
+  useEffect(() => {
+    setPlayers({ sortid: "none", attendees: attendees });
+  }, [attendees]);
+
+  type attendeeType = NonNullable<typeof attendees>[number];
+  const handleSort = (
+    sortid: string,
+    sortFn: (a: attendeeType, b: attendeeType) => number
+  ) => {
+    setPlayers((prev) => {
+      if (prev.sortid === sortid) {
+        // slice is here because of react weird behavior
+        return { sortid, attendees: prev.attendees?.slice().reverse() };
+      }
+
+      return {
+        sortid,
+        attendees: prev.attendees?.sort((a, b) => sortFn(a, b)),
+      };
+    });
+  };
 
   return (
     <div className="mb-32 overflow-hidden rounded border md:relative">
       <Table>
-        {/* {isLoading && <TableCaption>Loading..</TableCaption>} */}
         <TableHeader>
           <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead className="hidden md:table-cell">Email</TableHead>
-            <TableHead className="hidden md:table-cell">
-              <p className="flex items-center gap-1">
-                Member <ChevronsUpDown size={16} />
-              </p>
+            <TableHead>
+              <button
+                className="flex items-center gap-1"
+                onClick={() => handleSort("name", sortByName)}
+              >
+                Name <ChevronsUpDown size={16} />
+              </button>
             </TableHead>
             <TableHead className="hidden md:table-cell">
-              <p className="flex items-center gap-1">
+              <button
+                className="flex items-center gap-1"
+                onClick={() => handleSort("email", sortByEmail)}
+              >
+                Email <ChevronsUpDown size={16} />
+              </button>
+            </TableHead>
+            <TableHead className="hidden md:table-cell">
+              <button
+                className="flex items-center gap-1"
+                onClick={() => handleSort("member", sortByMember)}
+              >
+                Member <ChevronsUpDown size={16} />
+              </button>
+            </TableHead>
+            <TableHead className="hidden md:table-cell">
+              <button
+                className="flex items-center gap-1"
+                onClick={() => handleSort("playLevel", sortByPlayLevel)}
+              >
                 Play Level <ChevronsUpDown size={16} />
-              </p>
+              </button>
             </TableHead>
           </TableRow>
         </TableHeader>
@@ -65,54 +124,57 @@ export const AttendeesTable = ({
           {isLoading ? (
             <SkeletonAttendeeList />
           ) : (
-            attendees?.map((attendee, i) => (
-              <>
-                <TableRow key={i} className="hidden md:table-row">
-                  <TableCell className="place-items-center font-medium">
-                    {attendee.name}
-                    {attendee.pro && <Badge className="ml-2">Pro</Badge>}
-                  </TableCell>
-                  <TableCell className="hidden md:table-cell">
-                    {attendee.email}
-                  </TableCell>
-                  <TableCell className="hidden md:table-cell">
-                    {attendee.member ? "Yes" : "No"}
-                  </TableCell>
-                  <TableCell className="hidden md:table-cell">
-                    {attendee.playLevel}
-                  </TableCell>
-                </TableRow>
-                <Accordion
-                  type="single"
-                  collapsible
-                  className="w-full md:hidden"
-                >
-                  <AccordionItem value="item-1">
-                    <AccordionTrigger className="p-4">
-                      <p>
-                        {attendee.name}
-                        {attendee.pro && <Badge className="ml-2">Pro</Badge>}
-                      </p>
-                    </AccordionTrigger>
-                    <AccordionContent className="flex p-4">
-                      <div className="mr-1 flex flex-col">
-                        <strong>Email:</strong>
-                        <strong>Member:</strong>
-                        <strong>Level:</strong>
-                      </div>
-                      <div className="w-60 truncate">
-                        <p>{attendee.email} </p>
-                        <p>{attendee.member ? "Yes" : "No"}</p>
-                        <p>{attendee.playLevel}</p>
-                      </div>
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-              </>
+            sortedPlayers.attendees?.map((attendee, i) => (
+              <TableRow key={i} className="hidden md:table-row">
+                <TableCell className="place-items-center font-medium">
+                  {attendee.name}
+                  {attendee.pro && <Badge className="ml-2">Pro</Badge>}
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  {attendee.email}
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  {attendee.member ? "Yes" : "No"}
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  {attendee.playLevel}
+                </TableCell>
+              </TableRow>
             ))
           )}
         </TableBody>
       </Table>
+      {sortedPlayers.attendees?.map((attendee, i) => (
+        <Accordion
+          key={`accordian-${i}`}
+          type="single"
+          collapsible
+          className="md:hidden"
+        >
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="p-4">
+              <div>
+                {attendee.name}
+                {attendee.pro && <Badge className="ml-2">Pro</Badge>}
+              </div>
+            </AccordionTrigger>
+            <AccordionContent className="flex w-full truncate p-4">
+              <div className="mr-1 flex flex-col">
+                <strong>Email:</strong>
+                <strong>Member:</strong>
+                <strong>Level:</strong>
+              </div>
+              <div>
+                <p className="max-w-[200px] truncate xs:max-w-max">
+                  {attendee.email}{" "}
+                </p>
+                <p>{attendee.member ? "Yes" : "No"}</p>
+                <p>{attendee.playLevel}</p>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      ))}
     </div>
   );
 };
