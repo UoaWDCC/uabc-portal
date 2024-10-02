61af6335631317f99b3f9848341ec85ae4d602f0
Eric Zheng <eric.zheng@outlook.com>
2024-10-03 10:08:54 +1300

    refactor: tied up loose ends

--- src/hooks/mutations/reset-password.ts
+++ src/hooks/mutations/reset-password.ts
@@ -2,20 +2,13 @@ import { useMutation } from "@tanstack/react-query";
 
 export const useResetPasswordMutation = (resetPasswordToken: string) => {
   const mutation = useMutation({
-    mutationFn: async ({
-      newPassword,
-      confirmPassword,
-    }: {
-      newPassword: string;
-      confirmPassword: string;
-    }) => {
+    mutationFn: async (newPassword: string) => {
       const res = await fetch(
         `/api/auth/reset-password`,
         {
           method: "POST",
           body: JSON.stringify({
             newPassword,
-            confirmPassword,
             resetPasswordToken
           }),
           headers: {
