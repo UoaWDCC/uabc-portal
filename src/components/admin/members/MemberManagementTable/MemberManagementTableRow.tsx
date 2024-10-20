88a84589bd37199c5dd923d06d15e08a76995f19
Kimia Varasteh <kimiavarasteh@Kimias-Laptop.fritz.box>
2024-10-20 23:01:05 +1300

    fix: fix text alignments

--- src/components/admin/members/MemberManagementTable/MemberManagementTableRow.tsx
+++ src/components/admin/members/MemberManagementTable/MemberManagementTableRow.tsx
@@ -56,76 +56,23 @@ export function MemberManagementTableRow({
   const prepaidSessions: string = row.getValue("prepaidSessions");
 
   return (
-    // <Dialog>
-      <TableRow>
-        <TableCell className="w-[75px] max-w-[125px] truncate sm:max-w-max">
-          {name}
-        </TableCell>
-        <TableCell className="min-w-[100px] max-w-[150px] truncate xs:table-cell sm:max-w-full">
-          {email}
-        </TableCell>
-        <TableCell className="min-w-[100px] max-w-[150px] truncate xs:table-cell sm:max-w-full">
-          {prepaidSessions}
-        </TableCell>
-        <TableCell className="p-4">
-          <div className="flex h-10 items-center justify-center">
-            {/* <DialogTrigger asChild> */}
-              <Button className="h-6 w-8" variant="outline">
-                <Ellipsis className="absolute w-4 stroke-tertiary" />
-              </Button>
-            {/* </DialogTrigger> */}
-          </div>
-        </TableCell>
-      </TableRow>
+    <TableRow>
+      <TableCell className="w-[75px] max-w-[125px] truncate sm:max-w-max">
+        {name}
+      </TableCell>
+      <TableCell className="min-w-[100px] max-w-[150px] truncate xs:table-cell sm:max-w-full">
+        {email}
+      </TableCell>
+      <TableCell className="min-w-[100px] max-w-[150px] text-left">
+        {prepaidSessions}
+      </TableCell>
+      <TableCell className="p-4 text-center">
+        <div className="flex h-10 items-center justify-left">
+          <Button className="h-6 w-8" variant="outline">
+            <Ellipsis className="absolute w-4 stroke-tertiary" />
+          </Button>
+        </div>
+      </TableCell>
+    </TableRow>
   );
 }
-{/* <DialogContent className="w-full overflow-hidden">
-        <DialogHeader>
-          <DialogTitle>Approve Member</DialogTitle>
-        </DialogHeader>
-        <div className="flex w-full flex-col overflow-hidden text-foreground">
-          <Card className="w-full overflow-hidden p-2" variant="card">
-            <div className="w-full text-sm">
-              <p className="whitespace-nowrap font-medium">Member Details</p>
-              <div className="mt-1 flex w-full">
-                <div className="mr-4 flex-shrink-0 whitespace-nowrap">
-                  <p>First Name</p>
-                  <p>Last Name</p>
-                  <p>Email</p>
-                </div>
-                <div className="grow overflow-hidden *:truncate">
-                  <p>{firstName}</p>
-                  <p>{lastName}</p>
-                  <p>{email}</p>
-                </div>
-              </div>
-            </div>
-          </Card>
-          <form onSubmit={handleSubmit(handleApproveClick)} className="my-4">
-            <TextInput
-              type="number"
-              className="h-10 w-full"
-              {...register("prepaidSessions")}
-              label="Prepaid Sessions"
-              isError={!!errors.prepaidSessions}
-              errorMessage={errors.prepaidSessions?.message}
-            />
-            <input type="submit" id={userId} className="hidden" />
-          </form>
-          <DialogFooter className="flex gap-2">
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button variant="destructive" onClick={handleRejectClick}>
-              Reject
-            </Button>
-            <Button
-              variant="default"
-              onClick={handleSubmit(handleApproveClick)}
-            >
-              Approve
-            </Button>
-          </DialogFooter>
-        </div>
-      </DialogContent>
-    </Dialog> */}
